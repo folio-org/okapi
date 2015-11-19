ead01fbe9fcaaa063729a611d13d64d77cecb1fb
Adam Dickmeiss <adam@indexdata.dk>
2015-11-19 16:40:51 +0100

    ProcessModuleHandle.start is really non-blocking
    It uses executeBlocking. What a pain this was, but the result is ok.

--- sling-core/src/test/java/com/indexdata/sling/conduit/ProcessModuleHandleTest.java
+++ sling-core/src/test/java/com/indexdata/sling/conduit/ProcessModuleHandleTest.java
@@ -11,10 +11,12 @@ import org.junit.Test;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.junit.After;
 import org.junit.Assert;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -34,8 +36,15 @@ public class ProcessModuleHandleTest {
     vertx = Vertx.vertx();
   }
   
+   @After
+  public void tearDown(TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+    System.out.println("closing down");
+  }
+
   @Test
   public void test1(TestContext context) {
+    final Async async = context.async();
     ProcessDeploymentDescriptor desc = new ProcessDeploymentDescriptor();
     desc.cmdline_start = "sleep 10";
     ProcessModuleHandle pmh = new ProcessModuleHandle(desc);
@@ -43,16 +52,29 @@ public class ProcessModuleHandleTest {
   
     mh.init(vertx);
     mh.start(res -> {
-      assertTrue(res.succeeded());
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException ex) {
-        fail(ex.getMessage());
+      context.assertTrue(res.succeeded());
+      if (!res.succeeded()) {
+        async.complete();
+        return;        
       }
       mh.stop(res2 -> {
-        assertTrue(res2.succeeded());
-        context.async().complete();
+        context.assertTrue(res2.succeeded());
+        async.complete();
       });
+    });
+  }
+  @Test
+  public void test2(TestContext context) {
+    final Async async = context.async();
+    ProcessDeploymentDescriptor desc = new ProcessDeploymentDescriptor();
+    desc.cmdline_start = "sleepxxx 10";
+    ProcessModuleHandle pmh = new ProcessModuleHandle(desc);
+    ModuleHandle mh = pmh;
+  
+    mh.init(vertx);
+    mh.start(res -> {
+      context.assertFalse(res.succeeded());
+      async.complete();
     });
   }
 }
