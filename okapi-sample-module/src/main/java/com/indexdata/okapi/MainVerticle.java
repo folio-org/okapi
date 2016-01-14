867f0ac456f6db6adcb839771c56fb634707c105
Nassib Nassar <nn@nassarsoftware.com>
2016-01-14 14:59:48 -0500

    Rename sling in okapi-sample-module

--- okapi-sample-module/src/main/java/com/indexdata/okapi/MainVerticle.java
+++ okapi-sample-module/src/main/java/com/indexdata/okapi/MainVerticle.java
@@ -3,7 +3,7 @@
  * All rights reserved.
  * See the file LICENSE for details.
  */
-package com.indexdata.sling;
+package com.indexdata.okapi;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
