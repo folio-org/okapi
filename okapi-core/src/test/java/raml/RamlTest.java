518710f3d17a545175eacba3a8b411eb328c8436
Adam Dickmeiss <adam@indexdata.dk>
2016-02-29 13:42:29 +0100

    Unused imports

--- okapi-core/src/test/java/raml/RamlTest.java
+++ okapi-core/src/test/java/raml/RamlTest.java
@@ -5,19 +5,13 @@
  */
 package raml;
 
-import java.io.InputStream;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.raml.model.Raml;
 import org.raml.parser.rule.ValidationResult;
-import org.raml.parser.loader.DefaultResourceLoader;
-import org.raml.parser.loader.ResourceLoader;
-import org.raml.parser.visitor.RamlDocumentBuilder;
 import org.raml.parser.visitor.RamlValidationService;
 
 public class RamlTest {
