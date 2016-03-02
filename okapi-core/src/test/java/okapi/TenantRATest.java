7dfcb5465cb57b47e95d48668fba2ccd98902966
Adam Dickmeiss <adam@indexdata.dk>
2016-03-02 09:31:24 +0100

    Add test to delete tenant
    This is because - apparently issue #56 was fixed by
    commit 7d008b18c02a6abbd701c126c1b827dc771d0793

--- okapi-core/src/test/java/okapi/TenantRATest.java
+++ okapi-core/src/test/java/okapi/TenantRATest.java
@@ -43,7 +43,7 @@ public class TenantRATest {
     int port = Integer.parseInt(System.getProperty("port", "9130"));
 
     given().port(port).get("/_/tenants").then().statusCode(200).body(equalTo("[ ]"));
-    
+
     String doc1 = "{"+LS
             + "  \"name\" : \"roskilde\","+LS
             + "  \"description\" : \"Roskilde bibliotek\""+LS
@@ -77,6 +77,6 @@ public class TenantRATest {
 
     given().port(port).get("/_/tenants").then().statusCode(200).body(equalTo("[ " + doc3 + " ]"));
 
-    // given().port(port).delete(location3).then().statusCode(204);
+    given().port(port).delete(location3).then().statusCode(204);
   }
 }
