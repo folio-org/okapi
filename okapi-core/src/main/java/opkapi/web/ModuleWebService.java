folio-org/okapi

commit a742a9f944789a525574835b08494c99021db53d

Use Use nidi3/raml-tester for parts of tenant tests
And fix a few missing things in okapi.raml. There are several
errors cases not covered in our RAML and also we need to use
it for module tests

okapi-core/src/main/java/opkapi/web/ModuleWebService.java
@@ -172,27 +172,31 @@ public class ModuleWebService {
     final String q = "{ \"id\": \"" + id + "\"}";
     JsonObject jq = new JsonObject(q);
     //cli.find(collection, jq, res -> {
-    moduleStore.get(id, res->{
+    moduleStore.get(id, res -> {
       if (res.succeeded()) {
-          ctx.response().setStatusCode(200).end(Json.encodePrettily(res.result()));
-      } else if ( res.getType() == NOT_FOUND ) {
-          ctx.response().setStatusCode(404).end(res.cause().getMessage());
+        ctx.response()
+                .setStatusCode(200)
+                .putHeader("Content-Type", "application/json")
+                .end(Json.encodePrettily(res.result()));
+      } else if (res.getType() == NOT_FOUND) {
+        ctx.response().setStatusCode(404).end(res.cause().getMessage());
       } else { // must be internal error then
-          ctx.response().setStatusCode(500).end(res.cause().getMessage());
+        ctx.response().setStatusCode(500).end(res.cause().getMessage());
       }
     });
   }
 
   public void list(RoutingContext ctx) {
-    moduleStore.getAll(res->{
+    moduleStore.getAll(res -> {
       if (res.succeeded()) {
-          List<ModuleDescriptorBrief> ml = new ArrayList<>(res.result().size());
-          for (ModuleDescriptor md : res.result()) {
-            ml.add(new ModuleDescriptorBrief(md));
-          }
+        List<ModuleDescriptorBrief> ml = new ArrayList<>(res.result().size());
+        for (ModuleDescriptor md : res.result()) {
+          ml.add(new ModuleDescriptorBrief(md));
+        }
         ctx.response()
-          .setStatusCode(200)
-          .end(Json.encodePrettily(ml));
+                .setStatusCode(209)
+                .putHeader("Content-Type", "application/json")
+                .end(Json.encodePrettily(ml));
       } else {
         ctx.response().setStatusCode(500).end(res.cause().getMessage());
       }

okapi-core/src/main/java/opkapi/web/TenantWebService.java
@@ -106,6 +106,7 @@ public class TenantWebService {
             final String s = Json.encodePrettily(t.getDescriptor());
             ctx.response()
               .setStatusCode(201)
+              .putHeader("Content-Type", "application/json")
               .putHeader("Location", uri)
               .end(s);
             sendReloadSignal(id, ts);
@@ -135,6 +136,7 @@ public class TenantWebService {
             final String s = Json.encodePrettily(t.getDescriptor());
             ctx.response()
               .setStatusCode(200)
+              .putHeader("Content-Type", "application/json")
               .end(s);
             sendReloadSignal(id, ts);
           } else { 
@@ -153,7 +155,10 @@ public class TenantWebService {
     tenantStore.listTenants(res->{
       if (res.succeeded()) {
         String s = Json.encodePrettily(res.result());
-        ctx.response().end(s);
+        ctx.response()
+                .setStatusCode(200)
+                .putHeader("Content-Type", "application/json")
+                .end(s);
       } else {
         ctx.response().setStatusCode(400).end(res.cause().getMessage());
       }
@@ -167,10 +172,15 @@ public class TenantWebService {
       if ( res.succeeded() ) {
         Tenant t = res.result();
         String s = Json.encodePrettily(t.getDescriptor());
-        ctx.response().end(s);
+        ctx.response()
+                .setStatusCode(200)
+                .putHeader("Content-Type", "application/json")
+                .end(s);
       } else {
         if ( res.getType() == NOT_FOUND )
-          ctx.response().setStatusCode(404).end(res.cause().getMessage());
+          ctx.response().setStatusCode(404)
+                  .putHeader("Content-Type", "text/plain")
+                  .end(res.cause().getMessage());
         else
           ctx.response().setStatusCode(500).end(res.cause().getMessage());
       }

okapi-core/src/main/raml/okapi.raml
@@ -58,7 +58,6 @@ schemas:
       responses:
         204:
 
-
 /_/tenants:
   post:
     description: create a new tenant
@@ -74,7 +73,14 @@ schemas:
         body:
           application/json:
             schema: TenantDescriptor
-
+  get:
+    description: list all tenants
+    responses:
+      200:
+        description: list of tenant in a brief format
+        body:
+          application/json:
+            schema: TenantModules
   /{tenant_id}:
     get:
       description: retrieve tenant
@@ -83,6 +89,10 @@ schemas:
           body:
             application/json:
               schema: TenantDescriptor
+        404:
+          description: not found
+          body:
+            text/plain:
     put:
       description: update tenant
       responses:

okapi-core/src/test/java/okapi/TenantRATest.java
@@ -14,8 +14,12 @@ import org.junit.Test;
 import static com.jayway.restassured.RestAssured.*;
 import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
 import com.jayway.restassured.response.Response;
+import guru.nidi.ramltester.RamlDefinition;
+import guru.nidi.ramltester.RamlLoaders;
+import guru.nidi.ramltester.restassured.RestAssuredClient;
 import io.vertx.core.json.JsonObject;
 import static org.hamcrest.Matchers.*;
+import org.junit.Assert;
 
 public class TenantRATest {
 
@@ -45,7 +49,16 @@ public class TenantRATest {
 
     RestAssured.port = port;
 
-    given().get("/_/tenants").then().statusCode(200).body(equalTo("[ ]"));
+    RamlDefinition api = RamlLoaders.fromFile("src/main/raml").load("okapi.raml")
+            .assumingBaseUri("https://okapi.io");
+
+    RestAssuredClient c;
+
+    c = api.createRestAssured();
+    c.given().get("/_/tenants").then().statusCode(200).body(equalTo("[ ]"));
+    if (!c.getLastReport().isEmpty()) {
+       System.out.println(c.getLastReport().toString());
+    }
 
     String doc1 = "{"+LS
             + "  \"name\" : \"roskilde\","+LS
@@ -57,14 +70,44 @@ public class TenantRATest {
             + "  \"description\" : \"Roskilde bibliotek\""+LS
             + "}";
 
-    Response r = given().body(doc1).post("/_/tenants").then().statusCode(201).
-              body(equalTo(doc2)).extract().response();
+    c = api.createRestAssured();
+    Response r = c.given().header("Content-Type", "application/json").body(doc1).post("/_/tenants")
+            .then().statusCode(201).
+             body(equalTo(doc2)).extract().response();
+    if (!c.getLastReport().isEmpty()) {
+       System.out.println(c.getLastReport().toString());
+    }
+    Assert.assertTrue(c.getLastReport().isEmpty());
     String location = r.getHeader("Location");
 
-    given().get(location).then().statusCode(200).body(equalTo(doc2));
-    given().get(location + "none").then().statusCode(404);
-    given().get("/_/tenants").then().statusCode(200).body(equalTo("[ " + doc2 + " ]"));
-    given().delete(location).then().statusCode(204);
+    c = api.createRestAssured();
+    c.given().get(location).then().statusCode(200).body(equalTo(doc2));
+    if (!c.getLastReport().isEmpty()) {
+       System.out.println(c.getLastReport().toString());
+    }
+    Assert.assertTrue(c.getLastReport().isEmpty());
+
+    c = api.createRestAssured();
+    c.given().get(location + "none").then().statusCode(404);
+    if (!c.getLastReport().isEmpty()) {
+       System.out.println(c.getLastReport().toString());
+    }
+    Assert.assertTrue(c.getLastReport().isEmpty());
+
+    c = api.createRestAssured();
+    c.given().get("/_/tenants").then().statusCode(200).body(equalTo("[ " + doc2 + " ]"));
+    if (!c.getLastReport().isEmpty()) {
+       System.out.println(c.getLastReport().toString());
+    }
+    Assert.assertTrue(c.getLastReport().isEmpty());
+
+    c = api.createRestAssured();
+    c.given().delete(location).then().statusCode(204);
+    if (!c.getLastReport().isEmpty()) {
+       System.out.println(c.getLastReport().toString());
+    }
+    Assert.assertTrue(c.getLastReport().isEmpty());
+
     given().get("/_/tenants").then().statusCode(200).body(equalTo("[ ]"));
 
     String doc3 = "{"+LS

commit 0f0cabe8342294718258623cc54f1b326d549b65

Fix incorrect status code
This bad code (209) was introduced in recent commit a742a9f944789a5255.

okapi-core/src/main/java/opkapi/web/ModuleWebService.java
@@ -194,7 +194,7 @@ public class ModuleWebService {
           ml.add(new ModuleDescriptorBrief(md));
         }
         ctx.response()
-                .setStatusCode(209)
+                .setStatusCode(200)
                 .putHeader("Content-Type", "application/json")
                 .end(Json.encodePrettily(ml));
       } else {