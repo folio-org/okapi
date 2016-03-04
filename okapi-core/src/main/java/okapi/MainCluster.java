9b54b6c648bf6452ad04fa26d9bb62dcf88a5f64
Jakub Skoczen <jakub@indexdata.dk>
2016-03-04 15:55:15 +0100

    Configure vertx/hazelcast to use SLF4J
    instead of JUL

--- okapi-core/src/main/java/okapi/MainCluster.java
+++ okapi-core/src/main/java/okapi/MainCluster.java
@@ -5,14 +5,20 @@
  */
 package okapi;
 
+import com.hazelcast.config.Config;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 
 public class MainCluster {
   public static void main(String[] args) {
-    VertxOptions vopt = new VertxOptions();
-
+    System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
+    Config hazelcastConfig = new Config();
+    hazelcastConfig.setProperty("hazelcast.logging.type", "slf4j");
+    ClusterManager mgr = new HazelcastClusterManager(hazelcastConfig);
+    VertxOptions vopt = new VertxOptions().setClusterManager(mgr);
     Vertx.clusteredVertx(vopt, res->{
       if ( res.succeeded() ) {
         DeploymentOptions opt = new DeploymentOptions();
