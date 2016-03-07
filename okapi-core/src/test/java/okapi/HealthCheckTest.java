9758128cc742f55304b11a06c63a60cff1eabe8c
Heikki Levanto <heikki@indexdata.dk>
2016-03-07 14:46:58 +0100

    Async setUp and tearDown

--- okapi-core/src/test/java/okapi/HealthCheckTest.java
+++ okapi-core/src/test/java/okapi/HealthCheckTest.java
@@ -13,24 +13,32 @@ import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import static com.jayway.restassured.RestAssured.*;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.runner.RunWith;
 
+@RunWith(VertxUnitRunner.class)
 public class HealthCheckTest {
   Vertx vertx;
 
   private final int port = Integer.parseInt(System.getProperty("port", "9130"));
 
   @Before
-  public void setUp() {
+  public void setUp(TestContext context) {
     vertx = Vertx.vertx();
 
     DeploymentOptions opt = new DeploymentOptions()
             .setConfig(new JsonObject().put("storage", "inmemory"));
-    vertx.deployVerticle(MainVerticle.class.getName(), opt);
+    vertx.deployVerticle(MainVerticle.class.getName(), opt, context.asyncAssertSuccess());
   }
 
   @After
-  public void tearDown() {
-    vertx.close();
+  public void tearDown(TestContext context) {
+    Async async = context.async();
+    vertx.close(x -> {
+      async.complete();
+    });
   }
 
   @Test
