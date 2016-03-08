4d3638510cded6ed728886fc24f65c138df9157c
Heikki Levanto <heikki@indexdata.dk>
2016-03-08 10:51:44 +0100

    Cleaning whitespace and unused imports

--- okapi-sample-module/src/main/java/okapi/MainVerticle.java
+++ okapi-sample-module/src/main/java/okapi/MainVerticle.java
@@ -7,14 +7,9 @@ package okapi;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import static io.vertx.core.Vertx.vertx;
 import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.json.Json;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.handler.BodyHandler;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 
@@ -24,8 +19,9 @@ public class MainVerticle extends AbstractVerticle {
     ctx.response().setStatusCode(200);
     final String ctype = ctx.request().headers().get("Content-Type");
     String xmlMsg = "";
-    if ( ctype != null && ctype.toLowerCase().contains("xml"))
+    if (ctype != null && ctype.toLowerCase().contains("xml")) {
       xmlMsg = " (XML) ";
+    }
     final String xmlMsg2 = xmlMsg;
     //System.out.println("Sample: ctype='" + ctype + "'");
     if (ctx.request().method().equals(HttpMethod.GET)) {
@@ -51,28 +47,27 @@ public class MainVerticle extends AbstractVerticle {
     final int port = Integer.parseInt(System.getProperty("port", "8080"));
     System.out.println("Started sample-module on port " + port);
     //enable reading body to string
-    
+
     router.get("/sample").handler(this::my_stream_handle);
     router.post("/sample").handler(this::my_stream_handle);
 
     vertx.createHttpServer()
-            .requestHandler(router::accept)
-            .listen(
-                    port,
-                    result -> {
-                      if (result.succeeded()) {
-                        System.out.println("Okapi-Sample started PID "
-                          + ManagementFactory.getRuntimeMXBean().getName()
-                          + ". Listening on port " + port );
-                        fut.complete();
-                      } else {
-                        fut.fail(result.cause());
-                        System.out.println("Okapi-Sample failed to start listening! " + result.cause());
-                        //vertx.close();
-                      }
-                    }
-            );
+      .requestHandler(router::accept)
+      .listen(
+        port,
+        result -> {
+          if (result.succeeded()) {
+            System.out.println("Okapi-Sample started PID "
+              + ManagementFactory.getRuntimeMXBean().getName()
+              + ". Listening on port " + port);
+            fut.complete();
+          } else {
+            fut.fail(result.cause());
+            System.out.println("Okapi-Sample failed to start listening! " + result.cause());
+            //vertx.close();
+          }
+        }
+      );
   }
-
 
 }
