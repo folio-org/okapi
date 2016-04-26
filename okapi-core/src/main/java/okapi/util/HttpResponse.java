5fb76e9389cf0e4b64f726786846f114ba34f48d
Adam Dickmeiss <adam@indexdata.dk>
2016-04-26 10:29:28 +0200

    Add responseError: takes ErrorType and maps to HTTP error

--- okapi-core/src/main/java/okapi/util/HttpResponse.java
+++ okapi-core/src/main/java/okapi/util/HttpResponse.java
@@ -9,6 +9,17 @@ import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.RoutingContext;
 
 public class HttpResponse {
+  static public void responseError(RoutingContext ctx, ErrorType t, Throwable cause) {
+    int code = 500;
+    switch (t) {
+      case OK: code = 200; break;
+      case INTERNAL: code = 500; break;
+      case USER: code = 400; break;
+      case NOT_FOUND: code = 404; break;
+      case ANY: code = 500; break;
+    }
+    responseError(ctx, code, cause);
+  }
 
   static public void responseError(RoutingContext ctx, int code, Throwable cause) {
     responseText(ctx, code).end(cause.getMessage());
