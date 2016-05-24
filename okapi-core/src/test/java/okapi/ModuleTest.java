84830f0cb36bec08998fc6ea00574b995c8781a4
Adam Dickmeiss <adam@indexdata.dk>
2016-05-24 16:08:28 +0200

    Add/test /_/discovery/modules RAML

--- okapi-core/src/test/java/okapi/ModuleTest.java
+++ okapi-core/src/test/java/okapi/ModuleTest.java
@@ -519,6 +519,20 @@ public class ModuleTest {
     final String locationSample2Discovery = r.header("Location");
 
     c = api.createRestAssured();
+    c.given().get("/_/discovery/modules/sample-module2")
+            .then().statusCode(200)
+            .log().ifError();
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
+
+    c = api.createRestAssured();
+    c.given().get("/_/discovery/modules/sample-module2/sample2-inst")
+            .then().statusCode(200)
+            .log().ifError();
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
+
+    c = api.createRestAssured();
     c.given().get("/_/discovery/health")
             .then().statusCode(200);
     Assert.assertTrue("raml: " + c.getLastReport().toString(),
@@ -677,11 +691,12 @@ public class ModuleTest {
             .body(equalTo(exp3Modules));
     Assert.assertTrue(c.getLastReport().isEmpty());
 
-    // XXX
-    logger.info("Listing modules for the first time");
-    given().get("/_/discovery/modules")
+    c = api.createRestAssured();
+    c.given().get("/_/discovery/modules")
             .then().statusCode(200)
             .log().ifError();
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
 
     // make sample 2 disappear from discovery!
     c = api.createRestAssured();
@@ -690,11 +705,12 @@ public class ModuleTest {
     Assert.assertTrue("raml: " + c.getLastReport().toString(),
             c.getLastReport().isEmpty());
 
-    // XXX
-    logger.info("Listing modules for the second time");
-    given().get("/_/discovery/modules")
+    c = api.createRestAssured();
+    c.given().get("/_/discovery/modules")
             .then().statusCode(200)
             .log().ifError();
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
 
     given().header("X-Okapi-Tenant", okapiTenant)
             .header("X-Okapi-Token", okapiToken)
