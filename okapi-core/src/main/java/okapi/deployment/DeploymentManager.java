folio-org/okapi

commit 9523a23e895bf6d3dd4ca6d5ed2a00ffd75565eb

Add shutdown hook for deployment
so that processes , etc are killed and discovery is
informed about deployment node going down.

okapi-core/src/main/java/okapi/deployment/DeploymentManager.java
@@ -22,9 +22,11 @@ import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.Json;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import okapi.bean.DeploymentDescriptor;
 import okapi.bean.NodeDescriptor;
 import okapi.util.ModuleHandle;
@@ -68,6 +70,31 @@ public class DeploymentManager {
         fut.handle(new Success<>());
       }
     });
+  }
+
+  public void shutdown(Handler<ExtendedAsyncResult<Void>> fut) {
+    NodeDescriptor nd = new NodeDescriptor();
+    nd.setUrl("http://" + host + ":" + listenPort);
+    nd.setNodeId(host);
+    final String s = Json.encodePrettily(nd);
+    eb.send(DEPLOYMENT_NODE_STOP.toString(), s);
+
+    Iterator<String> iterator = list.keySet().iterator();
+    shutdown(iterator, fut);
+  }
+
+  private void shutdown(Iterator<String> it, Handler<ExtendedAsyncResult<Void>> fut) {
+    if (!it.hasNext()) {
+      fut.handle(new Success<>());
+    } else {
+      undeploy(it.next(), res -> {
+        if (res.failed()) {
+          fut.handle(new Failure<>(res.getType(), res.cause()));
+        } else {
+          shutdown(it, fut);
+        }
+      });
+    }
   }
 
   public void deploy(DeploymentDescriptor md1, Handler<ExtendedAsyncResult<DeploymentDescriptor>> fut) {

okapi-core/src/main/java/okapi/MainVerticle.java
@@ -142,7 +142,7 @@ public class MainVerticle extends AbstractVerticle {
     Runtime.getRuntime().addShutdownHook(new Thread() {
       public void run() {
         CountDownLatch latch = new CountDownLatch(1);
-        moduleManager.deleteAll(ar -> {
+        deploymentManager.shutdown(ar -> {
           latch.countDown();
         });
         try {
@@ -154,6 +154,7 @@ public class MainVerticle extends AbstractVerticle {
         }
       }
     });
+
   }
 
   public void NotFound(RoutingContext ctx) {

okapi-core/src/main/java/okapi/service/ModuleManager.java
@@ -36,7 +36,6 @@ public class ModuleManager {
 
   LinkedHashMap<String, ModuleDescriptor> modules = new LinkedHashMap<>();
 
-
   public ModuleManager(Vertx vertx) {
     String metricKey = "modules.count";
     DropwizardHelper.registerGauge(metricKey, () -> modules.size());
@@ -44,53 +43,56 @@ public class ModuleManager {
   }
 
   private boolean checkOneDependency(ModuleDescriptor md, ModuleInterface req,
-      Handler<ExtendedAsyncResult<String>> fut){
+          Handler<ExtendedAsyncResult<String>> fut) {
     ModuleInterface seenversion = null;
     for (String runningmodule : modules.keySet()) {
       ModuleDescriptor rm = modules.get(runningmodule);
       ModuleInterface[] provides = rm.getProvides();
-      if ( provides != null ) {
-        for ( ModuleInterface pi: provides ) {
+      if (provides != null) {
+        for (ModuleInterface pi : provides) {
           logger.debug("Checking dependency of " + md.getId() + ": "
-              + req.getId() + " " + req.getVersion()
-              + " against " + pi.getId() + " " + pi.getVersion() );
-          if ( req.getId().equals(pi.getId())) {
-            if ( seenversion == null || pi.compare(req) > 0)
+                  + req.getId() + " " + req.getVersion()
+                  + " against " + pi.getId() + " " + pi.getVersion());
+          if (req.getId().equals(pi.getId())) {
+            if (seenversion == null || pi.compare(req) > 0) {
               seenversion = pi;
-            if ( pi.isCompatible(req))
+            }
+            if (pi.isCompatible(req)) {
               return true;
+            }
           }
         }
       }
     }
-    if (  seenversion == null ) {
+    if (seenversion == null) {
       logger.debug("Can not create module '" + md.getId() + "'"
-        +", missing dependency " + req.getId() + ": " + req.getVersion() );
+              + ", missing dependency " + req.getId() + ": " + req.getVersion());
       fut.handle(new Failure<>(USER, "Can not create module '" + md.getId() + "'. "
-        + "Missing dependency: " +  req.getId() + ": " + req.getVersion()));
+              + "Missing dependency: " + req.getId() + ": " + req.getVersion()));
     } else {
       logger.debug("Can not create module '" + md.getId() + "'"
-        + "Insufficient version for " + req.getId() + ". "
-        + "Need " + req.getVersion() + ". have " + seenversion.getVersion() );
+              + "Insufficient version for " + req.getId() + ". "
+              + "Need " + req.getVersion() + ". have " + seenversion.getVersion());
       fut.handle(new Failure<>(USER, "Can not create module '" + md.getId() + "'"
-        + "Insufficient version for " + req.getId() + ". "
-        + "Need " + req.getVersion() + ". have " + seenversion.getVersion()));
+              + "Insufficient version for " + req.getId() + ". "
+              + "Need " + req.getVersion() + ". have " + seenversion.getVersion()));
     }
     return false;
   }
 
   /**
    * Check that the dependencies are satisfied.
+   *
    * @param md Module to be created
    * @param fut to be called in case of failure
    * @return true if no problems
    */
   private boolean checkDependencies(ModuleDescriptor md,
-        Handler<ExtendedAsyncResult<String>> fut) {
+          Handler<ExtendedAsyncResult<String>> fut) {
     ModuleInterface[] requires = md.getRequires();
     if (requires != null) {
       for (ModuleInterface req : requires) {
-        if ( ! checkOneDependency(md, req, fut)) {
+        if (!checkOneDependency(md, req, fut)) {
           return false;
         }
       }
@@ -142,6 +144,5 @@ public class ModuleManager {
   public Set<String> list() {
     return modules.keySet();
   }
-
 
 } // class

commit b175d1d12b7757903e62c01e190ed67b59c3dab2

Fix concurrency issue with undeployment
Introduced by bad code in 9523a23e895bf6d3dd4ca6d5ed2a00ffd75565eb

okapi-core/src/main/java/okapi/deployment/DeploymentManager.java
@@ -78,12 +78,11 @@ public class DeploymentManager {
     nd.setNodeId(host);
     final String s = Json.encodePrettily(nd);
     eb.send(DEPLOYMENT_NODE_STOP.toString(), s);
-
-    Iterator<String> iterator = list.keySet().iterator();
-    shutdown(iterator, fut);
+    shutdownR(fut);
   }
 
-  private void shutdown(Iterator<String> it, Handler<ExtendedAsyncResult<Void>> fut) {
+  private void shutdownR(Handler<ExtendedAsyncResult<Void>> fut) {
+    Iterator<String> it = list.keySet().iterator();
     if (!it.hasNext()) {
       fut.handle(new Success<>());
     } else {
@@ -91,7 +90,7 @@ public class DeploymentManager {
         if (res.failed()) {
           fut.handle(new Failure<>(res.getType(), res.cause()));
         } else {
-          shutdown(it, fut);
+          shutdownR(fut);
         }
       });
     }