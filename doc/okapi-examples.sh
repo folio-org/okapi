7441a5e3fe1f9725e4c6bd9b3decec15a681ab84
Heikki Levanto <heikki@indexdata.dk>
2016-07-22 10:38:51 +0200

    Make okapi-examples use the actual examples in the guide
    Pasting these manually was tedious. Now it greps the examples
    out of the guide, so they need to be updated only once.

--- doc/okapi-examples.sh
+++ doc/okapi-examples.sh
@@ -1,213 +1,33 @@
 #!/bin/bash
 #
 # Simple script to set up Okapi with modules, tenants, etc
-# Pasted from the guide.md
-
+# Extracts the examples from guide.md
+#
 # You should be in the main okapi directory, typically ~/proj/okapi
-
-
-#########
-# Okapi itself
-
-OKAPI=${1:-"http://localhost:9130"}
-# The usual place it runs on a single-machine setup
-
-# Start up Okapi itself - in a different console window:
-# java -jar okapi-core/target/okapi-core-fat.jar dev
-
-# See that Okapi is running
-curl -w '\n' $OKAPI/_/proxy/tenants
-
+#
+# Okapi should be running in a separate console window, start it with
+#     java -jar okapi-core/target/okapi-core-fat.jar dev
 
 ########
-# Deploy the sample module
+# Parameters
 
-cat > /tmp/sampledeploy.json <<END
-{
-  "srvcId" : "sample-module",
-  "descriptor" : {
-    "exec" : "java -Dport=%p -jar okapi-sample-module/target/okapi-sample-module-fat.jar"
-   }
-}
-END
-
-curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -d @/tmp/sampledeploy.json  \
-  $OKAPI/_/deployment/modules
-
-# Get deployment details for the sample
-curl -s -o /tmp/samplediscovery.json \
-  $OKAPI/_/deployment/modules/localhost-9131
-
-# And tell the proxy about it
-cat > /tmp/sampleproxy.json <<END
-  {
-    "id" : "sample-module",
-    "name" : "okapi sample module",
-    "provides" : [ {
-      "id" : "sample",
-      "version" : "1.2.3"
-    } ],
-    "routingEntries" : [ {
-      "methods" : [ "GET", "POST" ],
-      "path" : "/sample",
-      "level" : "30",
-      "type" : "request-response",
-      "permissionsRequired" : [ "sample.needed" ],
-      "permissionsDesired" : [ "sample.extra" ]
-    } ]
-  }
-END
-
-curl -w '\n' -X POST -D -   \
-    -H "Content-type: application/json"   \
-    -d @/tmp/sampleproxy.json \
-   $OKAPI/_/proxy/modules
+OKAPI=${1:-"http://localhost:9130"}   # The usual place it runs on a single-machine setup
+GUIDE=${2:-"doc/guide.md"}  # Where to find the guide
 
 
-#########
-# The auth module
+# Check that Okapi is running
+curl -w '\n' $OKAPI/_/proxy/tenants || exit 1
 
-cat > /tmp/authdeploy.json <<END
-{
-  "srvcId" : "auth",
-  "descriptor" : {
-    "exec" : "java -Dport=%p -jar okapi-auth/target/okapi-auth-fat.jar"
-   }
-}
-END
+# Check that we have the guide
+cat $GUIDE >/dev/null || exit 1
 
-curl -w '\n' -D - -s \
-  -X POST \
-  -o /tmp/authdiscovery.json \
-  -H "Content-type: application/json" \
-  -d @/tmp/authdeploy.json  \
-  $OKAPI/_/deployment/modules
+# Extract the example JSON from the guide
+perl -n -e  'print if /^cat /../^END/;' $GUIDE  | sh
 
+# And run the example curl commands that actually talk to Okapi, excluding
+# the clean-up delete commands, so we leave Okapi fully loaded.
+perl -n -e  'print if /^curl /../http/; ' $GUIDE |
+  grep -v 8080 | grep -v DELETE |
+  sh -x
 
-cat > /tmp/authmodule.json <<END
-{
-  "id" : "auth",
-  "name" : "auth",
-  "provides" : [ {
-    "id" : "auth",
-    "version" : "3.4.5"
-  } ],
-  "requires" : [ {
-    "id" : "sample",
-    "version" : "1.2.0"
-  } ],
-  "routingEntries" : [ {
-    "methods" : [ "*" ],
-    "path" : "/",
-    "level" : "10",
-    "type" : "request-response"
-  }, {
-    "methods" : [ "POST" ],
-    "path" : "/login",
-    "level" : "20",
-    "type" : "request-response"
-  } ]
-}
-END
-
-curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -d @/tmp/authmodule.json  \
-  $OKAPI/_/proxy/modules
-
-
-#############
-# Tenants
-
-cat > /tmp/tenant1.json <<END
-{
-  "id" : "our",
-  "name" : "our library",
-  "description" : "Our Own Library"
-}
-END
-
-curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -d @/tmp/tenant1.json  \
-  $OKAPI/_/proxy/tenants
-
-cat > /tmp/tenant2.json <<END
-{
-  "id" : "other",
-  "name" : "otherlibrary",
-  "description" : "The Other Library"
-}
-END
-
-curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -d @/tmp/tenant2.json  \
-  $OKAPI/_/proxy/tenants
-
-###########
-# Enabling the sample for tenant1
-
-cat > /tmp/enabletenant1.json <<END
-{
-  "id" : "sample-module"
-}
-END
-
-curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -d @/tmp/enabletenant1.json  \
-  $OKAPI/_/proxy/tenants/our/modules
-
-curl -w '\n' -D -  \
-  -H "X-Okapi-Tenant: our" \
-  $OKAPI/sample
-
-
-################
-# Enabling sample and auth for tenant 2
-
-cat > /tmp/enabletenant2a.json <<END
-{
-  "id" : "sample-module"
-}
-END
-
-curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -d @/tmp/enabletenant2a.json  \
-  $OKAPI/_/proxy/tenants/other/modules
-
-cat > /tmp/enabletenant2b.json <<END
-{
-  "id" : "auth"
-}
-END
-
-curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -d @/tmp/enabletenant2b.json  \
-  $OKAPI/_/proxy/tenants/other/modules
-
-# Logging in as tenant2
-cat > /tmp/login.json <<END
-{
-  "tenant": "other",
-  "username": "peter",
-  "password": "peter-password"
-}
-END
-
-curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -H "X-Okapi-Tenant: other" \
-  -d @/tmp/login.json  \
-  $OKAPI/login
-
-
-curl -w '\n' -D -  \
-  -H "X-Okapi-Tenant: other" \
-  -H "X-Okapi-Token: other:peter:04415268d4170e95ec497077ad4cba3c" \
-  $OKAPI/sample
+# The last line of output should say "It works"
