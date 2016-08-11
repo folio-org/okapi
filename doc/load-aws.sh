577210684cd19210c448f92f082ce6c0eb3f5f38
David Crossley <dcrossley@indexgeo.com.au>
2016-08-11 13:09:48 +1000

    Follow rename of 'sample' module to 'testb'

--- doc/load-aws.sh
+++ doc/load-aws.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #
 # A script to put different kind of loads on our AWS installation, to feed some
-# numbers to Grafana
+# numbers to Grafana.
 
 # Assumes we have set things up with okapi-examples, or similar
 #OKAPI="http://localhost:9130"
 OKAPI="http://okapi-test.aws.indexdata.com:9130"
 #OKAPI="http://okapi-test1.aws.indexdata.com:9130"
 
-
-MODULE=${1:-sample}  # module to invoke
-TENANT=${2:-our}     # tenant to invoke for
+MODULE=${1:-'testb'}  # module to invoke, default: okapi-test-module (testb)
+TENANT=${2:-'our'}     # tenant to invoke for
 SLEEP=${3:-1}       # delay between invocations (sec)
 TOKEN=${4:-"X-Okapi-Token: other:peter:04415268d4170e95ec497077ad4cba3c"}
 #echo " M='$MODULE' T='$TENANT' S='$SLEEP' T='$TOKEN'"
@@ -18,7 +17,6 @@ TOKEN=${4:-"X-Okapi-Token: other:peter:04415268d4170e95ec497077ad4cba3c"}
 # Stop file - touch this to stop all instances of this script
 STOP=aws.stop
 rm -f $STOP
-
 
 while [ ! -f $STOP ]
 do
