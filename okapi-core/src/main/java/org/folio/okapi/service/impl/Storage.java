12c86234b4a52e34d80f118bf18fd6802cbe78c6
Adam Dickmeiss <adam@indexdata.dk>
2016-11-21 15:24:24 +0100

    Wrap storage handling in Storage class
    Constructor serves as factory function.

---
+++ okapi-core/src/main/java/org/folio/okapi/service/impl/Storage.java
@@ -0,0 +1,69 @@
+package org.folio.okapi.service.impl;
+
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import static org.folio.okapi.common.ErrorType.INTERNAL;
+import org.folio.okapi.common.ExtendedAsyncResult;
+import org.folio.okapi.common.Failure;
+import org.folio.okapi.common.Success;
+import org.folio.okapi.service.ModuleStore;
+import org.folio.okapi.service.TenantStore;
+import org.folio.okapi.service.TimeStampStore;
+
+public class Storage {
+
+  private MongoHandle mongo;
+  private ModuleStore moduleStore;
+  private TimeStampStore timeStampStore;
+  private TenantStore tenantStore;
+
+  private final Logger logger = LoggerFactory.getLogger("okapi");
+
+  public Storage(Vertx vertx, String type, JsonObject config) {
+    switch (type) {
+      case "mongo":
+        mongo = new MongoHandle(vertx, config);
+        moduleStore = new ModuleStoreMongo(mongo);
+        timeStampStore = new TimeStampMongo(mongo);
+        tenantStore = new TenantStoreMongo(mongo);
+        break;
+      case "inmemory":
+        moduleStore = new ModuleStoreMemory(vertx);
+        timeStampStore = new TimeStampMemory(vertx);
+        tenantStore = new TenantStoreMemory();
+        break;
+      default:
+        logger.fatal("Unknown storage type '" + type + "'");
+        System.exit(1);
+    }
+  }
+
+  public void resetDatabases(Handler<ExtendedAsyncResult<Void>> fut) {
+    if (mongo != null && mongo.isTransient()) {
+      mongo.dropDatabase(res -> {
+        if (res.succeeded()) {
+          fut.handle(new Success<>());
+        } else {
+          fut.handle(new Failure(INTERNAL, res.cause()));
+        }
+      });
+    } else {
+      fut.handle(new Success<>());
+    }
+  }
+
+  public ModuleStore geModuleStoret() {
+    return moduleStore;
+  }
+
+  public TenantStore getTenantStore() {
+    return tenantStore;
+  }
+
+  public TimeStampStore getTimeStampStore() {
+    return timeStampStore;
+  }
+}
