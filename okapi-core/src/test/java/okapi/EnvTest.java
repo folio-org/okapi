56f119272adade69aee683fd080fc84ff6f94d67
Adam Dickmeiss <adam@indexdata.dk>
2017-02-07 16:05:17 +0100

    Env tests

--- okapi-core/src/test/java/okapi/EnvTest.java
+++ okapi-core/src/test/java/okapi/EnvTest.java
@@ -2,6 +2,7 @@ package okapi;
 
 import org.folio.okapi.MainVerticle;
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import org.junit.After;
@@ -23,7 +24,7 @@ import org.junit.runner.RunWith;
 @RunWith(VertxUnitRunner.class)
 public class EnvTest {
 
-  private final Logger logger = LoggerFactory.getLogger("okapi.DeployModuleIntegration");
+  private final Logger logger = LoggerFactory.getLogger("okapi");
 
   Vertx vertx;
   private static final String LS = System.lineSeparator();
@@ -58,11 +59,71 @@ public class EnvTest {
             .assumingBaseUri("https://okapi.cloud");
 
     RestAssuredClient c;
+    Response r;
 
     c = api.createRestAssured();
     c.given().get("/_/env").then().statusCode(200).body(equalTo("[ ]"));
     Assert.assertTrue("raml: " + c.getLastReport().toString(),
-             c.getLastReport().isEmpty());
+            c.getLastReport().isEmpty());
 
+    c = api.createRestAssured();
+    c.given().get("/_/env/name1").then().statusCode(404);
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
+
+    final String badDoc = "{" + LS
+            + "  \"name\" : \"mame1\"," + LS // the comma here makes it bad json!
+            + "}";
+    c = api.createRestAssured();
+    c.given()
+            .header("Content-Type", "application/json")
+            .body(badDoc).post("/_/env")
+            .then().statusCode(400);
+
+    final String doc = "{" + LS
+            + "  \"name\" : \"name1\"," + LS
+            + "  \"value\" : \"value1\"" + LS
+            + "}";
+
+    c = api.createRestAssured();
+    r = c.given()
+            .header("Content-Type", "application/json")
+            .body(doc).post("/_/env")
+            .then()
+            .statusCode(201)
+            .body(equalTo(doc))
+            .extract().response();
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
+    String locationName1 = r.getHeader("Location");
+    c = api.createRestAssured();
+    c.given().get(locationName1)
+            .then()
+            .statusCode(200)
+            .body(equalTo(doc));
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
+
+    c = api.createRestAssured();
+    c.given().get("/_/env").then().statusCode(200);
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
+
+    c = api.createRestAssured();
+    c.given().delete(locationName1)
+            .then()
+            .statusCode(204);
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
+
+    c = api.createRestAssured();
+    c.given().get(locationName1).then().statusCode(404);
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
+
+    c = api.createRestAssured();
+    c.given().get("/_/env").then().statusCode(200).body(equalTo("[ ]"));
+    Assert.assertTrue("raml: " + c.getLastReport().toString(),
+            c.getLastReport().isEmpty());
   }
 }
