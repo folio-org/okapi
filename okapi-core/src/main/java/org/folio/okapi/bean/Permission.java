0d28e0d7985e0b3e56a57438de8a164c4e60adb9
David Crossley <dcrossley@indexgeo.com.au>
2017-03-03 11:44:07 +1100

    Remove superfluous license headers (OKAPI-109)

--- okapi-core/src/main/java/org/folio/okapi/bean/Permission.java
+++ okapi-core/src/main/java/org/folio/okapi/bean/Permission.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2015-2017, Index Data
- * All rights reserved.
- * See the file LICENSE for details.
- */
 package org.folio.okapi.bean;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
