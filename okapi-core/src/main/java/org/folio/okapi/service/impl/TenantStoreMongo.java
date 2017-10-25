38a44a600cfb889f36347e66c4ef343c75afc235
Adam Dickmeiss <adam@indexdata.dk>
2017-10-25 14:20:23 +0200

    Reformat

--- okapi-core/src/main/java/org/folio/okapi/service/impl/TenantStoreMongo.java
+++ okapi-core/src/main/java/org/folio/okapi/service/impl/TenantStoreMongo.java
@@ -61,8 +61,7 @@ public class TenantStoreMongo implements TenantStore {
   }
 
   @Override
-  public void insert(Tenant t,
-          Handler<ExtendedAsyncResult<String>> fut) {
+  public void insert(Tenant t, Handler<ExtendedAsyncResult<String>> fut) {
     String id = t.getId();
     JsonObject document = encodeTenant(t);
     document.put("_id", id);
