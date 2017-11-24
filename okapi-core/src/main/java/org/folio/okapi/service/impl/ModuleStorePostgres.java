d49f36690c412d66d2e4f48f832938398187344f
Adam Dickmeiss <adam@indexdata.dk>
2017-11-24 13:39:55 +0100

    Unused import and rename

--- okapi-core/src/main/java/org/folio/okapi/service/impl/ModuleStorePostgres.java
+++ okapi-core/src/main/java/org/folio/okapi/service/impl/ModuleStorePostgres.java
@@ -2,47 +2,44 @@ package org.folio.okapi.service.impl;
 
 import org.folio.okapi.service.ModuleStore;
 import io.vertx.core.Handler;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 import java.util.List;
 import org.folio.okapi.bean.ModuleDescriptor;
 import org.folio.okapi.common.ExtendedAsyncResult;
 
 public class ModuleStorePostgres implements ModuleStore {
 
-  private final Logger logger = LoggerFactory.getLogger("okapi");
   private static final String TABLE = "modules";
   private static final String JSON_COLUMN = "modulejson";
   private static final String ID_SELECT = JSON_COLUMN + "->>'id' = ?";
   private static final String ID_INDEX = JSON_COLUMN + "->'id'";
-  private final PostgresTable<ModuleDescriptor> table;
+  private final PostgresTable<ModuleDescriptor> pgTable;
 
   public ModuleStorePostgres(PostgresHandle pg) {
-    this.table = new PostgresTable(pg, TABLE, JSON_COLUMN, ID_INDEX, ID_SELECT, "module_id");
+    this.pgTable = new PostgresTable(pg, TABLE, JSON_COLUMN, ID_INDEX, ID_SELECT, "module_id");
   }
 
   @Override
   public void init(boolean reset, Handler<ExtendedAsyncResult<Void>> fut) {
-    table.init(reset, fut);
+    pgTable.init(reset, fut);
   }
 
   @Override
   public void insert(ModuleDescriptor md, Handler<ExtendedAsyncResult<Void>> fut) {
-    table.insert(md, fut);
+    pgTable.insert(md, fut);
   }
 
   @Override
   public void update(ModuleDescriptor md, Handler<ExtendedAsyncResult<Void>> fut) {
-    table.update(md, fut);
+    pgTable.update(md, fut);
   }
 
   @Override
   public void getAll(Handler<ExtendedAsyncResult<List<ModuleDescriptor>>> fut) {
-    table.getAll(ModuleDescriptor.class, fut);
+    pgTable.getAll(ModuleDescriptor.class, fut);
   }
 
   @Override
   public void delete(String id, Handler<ExtendedAsyncResult<Void>> fut) {
-    table.delete(id, fut);
+    pgTable.delete(id, fut);
   }
 }
