5a62ef79dd228c259b30c8372c693afd62bfb1a4
Adam Dickmeiss <adam@indexdata.dk>
2017-12-15 16:26:37 +0100

    Property ModuleTestStorage overrides storage for ModuleTest
    The storage names is separted by a blank. To run ModuleTest
    with inmemory and mongo, you could use:
    mvn -DModuleTestStorage="inmemory mongo" -Dtest=ModuleTest test

--- okapi-core/src/test/java/okapi/ModuleTest.java
+++ okapi-core/src/test/java/okapi/ModuleTest.java
@@ -56,6 +56,10 @@ public class ModuleTest {
 
   @Parameterized.Parameters
   public static Iterable<String> data() {
+    final String s = System.getProperty("ModuleTestStorage");
+    if (s != null) {
+      return Arrays.asList(s.split(" "));
+    }
     final String f = System.getenv("okapiFastTest");
     if (f != null) {
       return Arrays.asList("inmemory");
