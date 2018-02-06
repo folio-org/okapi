folio-org/okapi

commit 39c598e42ff772455cb0907db390a7263c1b04d4

Using CompositeFuture rather than recursion OKAPI-516

okapi-core/src/main/java/org/folio/okapi/discovery/DiscoveryManager.java
@@ -10,7 +10,6 @@ import io.vertx.core.json.Json;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.core.spi.cluster.NodeListener;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -46,13 +45,13 @@ public class DiscoveryManager implements NodeListener {
 
   private final Logger logger = OkapiLogger.get();
 
-  private LockedTypedMap2<DeploymentDescriptor> deployments = new LockedTypedMap2<>(DeploymentDescriptor.class);
-  private LockedTypedMap1<NodeDescriptor> nodes = new LockedTypedMap1<>(NodeDescriptor.class);
+  private final LockedTypedMap2<DeploymentDescriptor> deployments = new LockedTypedMap2<>(DeploymentDescriptor.class);
+  private final LockedTypedMap1<NodeDescriptor> nodes = new LockedTypedMap1<>(NodeDescriptor.class);
   private Vertx vertx;
   private ClusterManager clusterManager;
   private ModuleManager moduleManager;
   private HttpClient httpClient;
-  private DeploymentStore deploymentStore;
+  private final DeploymentStore deploymentStore;
 
   public void init(Vertx vertx, Handler<ExtendedAsyncResult<Void>> fut) {
     this.vertx = vertx;
@@ -465,34 +464,26 @@ public class DiscoveryManager implements NodeListener {
       } else {
         Collection<String> keys = resGet.result();
         List<DeploymentDescriptor> all = new LinkedList<>();
-        if (keys == null || keys.isEmpty()) {
-          fut.handle(new Success<>(all));
-        } else {
-          Iterator<String> it = keys.iterator();
-          getAllR(it, all, fut);
+        List<Future> futures = new LinkedList<>();
+        for (String s : keys) {
+          Future<List<DeploymentDescriptor>> f = Future.future();
+          this.get(s, f::handle);
+          if (f.succeeded()) {
+            all.addAll(f.result());
+          }
+          futures.add(f);
         }
+        CompositeFuture.all(futures).setHandler(res2 -> {
+          if (res2.failed()) {
+            fut.handle(new Failure<>(INTERNAL, res2.cause()));
+          } else {
+            fut.handle(new Success<>(all));
+          }
+        });
       }
     });
   }
 
-  private void getAllR(Iterator<String> it, List<DeploymentDescriptor> all,
-                       Handler<ExtendedAsyncResult<List<DeploymentDescriptor>>> fut) {
-
-    if (!it.hasNext()) {
-      fut.handle(new Success<>(all));
-    } else {
-      String srvcId = it.next();
-      this.get(srvcId, resGet -> {
-        if (resGet.failed()) {
-          fut.handle(new Failure<>(resGet.getType(), resGet.cause()));
-        } else {
-          List<DeploymentDescriptor> dpl = resGet.result();
-          all.addAll(dpl);
-          getAllR(it, all, fut);
-        }
-      });
-    }
-  }
   private void health(DeploymentDescriptor md,
     Handler<ExtendedAsyncResult<HealthDescriptor>> fut) {
 
@@ -526,23 +517,26 @@ public class DiscoveryManager implements NodeListener {
     }
   }
 
-  private void healthR(Iterator<DeploymentDescriptor> it,
-    List<HealthDescriptor> all,
+  private void healthList(List<DeploymentDescriptor> list,
     Handler<ExtendedAsyncResult<List<HealthDescriptor>>> fut) {
 
-    if (!it.hasNext()) {
-      fut.handle(new Success<>(all));
-    } else {
-      DeploymentDescriptor md = it.next();
-      health(md, res -> {
-        if (res.failed()) {
-          fut.handle(new Failure<>(res.getType(), res.cause()));
-        } else {
-          all.add(res.result());
-          healthR(it, all, fut);
-        }
-      });
+    List<HealthDescriptor> all = new LinkedList<>();
+    List<Future> futures = new LinkedList<>();
+    for (DeploymentDescriptor md : list) {
+      Future<HealthDescriptor> f = Future.future();
+      health(md, f::handle);
+      if (f.succeeded()) {
+        all.add(f.result());
+      }
+      futures.add(f);
     }
+    CompositeFuture.all(futures).setHandler(res2 -> {
+      if (res2.failed()) {
+        fut.handle(new Failure<>(INTERNAL, res2.cause()));
+      } else {
+        fut.handle(new Success<>(all));
+      }
+    });
   }
 
   public void health(Handler<ExtendedAsyncResult<List<HealthDescriptor>>> fut) {
@@ -550,9 +544,7 @@ public class DiscoveryManager implements NodeListener {
       if (res.failed()) {
         fut.handle(new Failure<>(res.getType(), res.cause()));
       } else {
-        Iterator<DeploymentDescriptor> it = res.result().iterator();
-        List<HealthDescriptor> all = new ArrayList<>();
-        healthR(it, all, fut);
+        healthList(res.result(), fut);
       }
     });
   }
@@ -572,9 +564,7 @@ public class DiscoveryManager implements NodeListener {
       if (res.failed()) {
         fut.handle(new Failure<>(res.getType(), res.cause()));
       } else {
-        Iterator<DeploymentDescriptor> it = res.result().iterator();
-        List<HealthDescriptor> all = new ArrayList<>();
-        healthR(it, all, fut);
+        healthList(res.result(), fut);
       }
     });
   }
@@ -670,25 +660,6 @@ public class DiscoveryManager implements NodeListener {
         });
       }
     });
-
-  }
-
-  private void getNodesR(Iterator<String> it, List<NodeDescriptor> all,
-                         Handler<ExtendedAsyncResult<List<NodeDescriptor>>> fut) {
-    if (!it.hasNext()) {
-      fut.handle(new Success<>(all));
-    } else {
-      String nodeId = it.next();
-      getNode1(nodeId, resGet -> {
-        if (resGet.failed()) {
-          fut.handle(new Failure<>(resGet.getType(), resGet.cause()));
-        } else {
-          NodeDescriptor dpl = resGet.result();
-          all.add(dpl);
-          getNodesR(it, all, fut);
-        }
-      });
-    }
   }
 
   public void getNodes(Handler<ExtendedAsyncResult<List<NodeDescriptor>>> fut) {
@@ -702,11 +673,22 @@ public class DiscoveryManager implements NodeListener {
           keys.retainAll(n);
         }
         List<NodeDescriptor> all = new LinkedList<>();
-        if (keys == null || keys.isEmpty()) {
-          fut.handle(new Success<>(all));
-        } else {
-          getNodesR(keys.iterator(), all, fut);
+        List<Future> futures = new LinkedList<>();
+        for (String nodeId : keys) {
+          Future<NodeDescriptor> f = Future.future();
+          getNode1(nodeId, f::handle);
+          if (f.succeeded()) {
+            all.add(f.result());
+          }
+          futures.add(f);
         }
+        CompositeFuture.all(futures).setHandler(res2 -> {
+          if (res2.failed()) {
+            fut.handle(new Failure<>(INTERNAL, res2.cause()));
+          } else {
+            fut.handle(new Success<>(all));
+          }
+        });
       }
     });
   }

commit 11daf497ab2bd578c33890f8fb9503af01fbbbe3

Fix result for a couple of async calls in discovery OKAPI-519
This problem was caused by 39c598e42ff772455cb0907db390a7263c1b04d4

okapi-core/src/main/java/org/folio/okapi/discovery/DiscoveryManager.java
@@ -467,10 +467,12 @@ public class DiscoveryManager implements NodeListener {
         List<Future> futures = new LinkedList<>();
         for (String s : keys) {
           Future<List<DeploymentDescriptor>> f = Future.future();
-          this.get(s, f::handle);
-          if (f.succeeded()) {
-            all.addAll(f.result());
-          }
+          this.get(s, res -> {
+            if (res.succeeded()) {
+              all.addAll(res.result());
+            }
+            f.handle(res);
+          });
           futures.add(f);
         }
         CompositeFuture.all(futures).setHandler(res2 -> {
@@ -524,10 +526,12 @@ public class DiscoveryManager implements NodeListener {
     List<Future> futures = new LinkedList<>();
     for (DeploymentDescriptor md : list) {
       Future<HealthDescriptor> f = Future.future();
-      health(md, f::handle);
-      if (f.succeeded()) {
-        all.add(f.result());
-      }
+      health(md, res -> {
+        if (res.succeeded()) {
+          all.add(res.result());
+        }
+        f.handle(res);
+      });
       futures.add(f);
     }
     CompositeFuture.all(futures).setHandler(res2 -> {
@@ -676,10 +680,12 @@ public class DiscoveryManager implements NodeListener {
         List<Future> futures = new LinkedList<>();
         for (String nodeId : keys) {
           Future<NodeDescriptor> f = Future.future();
-          getNode1(nodeId, f::handle);
-          if (f.succeeded()) {
-            all.add(f.result());
-          }
+          getNode1(nodeId, res -> {
+            if (res.succeeded()) {
+              all.add(res.result());
+            }
+            f.handle(res);
+          });
           futures.add(f);
         }
         CompositeFuture.all(futures).setHandler(res2 -> {