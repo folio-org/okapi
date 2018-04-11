179a9cc160e22b6393f6d0e6982c863c154d2545
Adam Dickmeiss <adam@indexdata.dk>
2018-04-11 13:26:02 +0200

    Remove unused import

--- okapi-core/src/main/java/org/folio/okapi/bean/ModuleDescriptor.java
+++ okapi-core/src/main/java/org/folio/okapi/bean/ModuleDescriptor.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import org.folio.okapi.util.ProxyContext;
