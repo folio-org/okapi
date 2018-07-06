169cb0d649284a882198e32e8edce8e5900a7fb7
tdolega <tymoteuszdolega@gmail.com>
2018-07-06 13:45:47 +0200

    initial messages utility

---
+++ okapi-common/src/test/java/org/folio/okapi/common/MessagesTest.java
@@ -0,0 +1,18 @@
+package org.folio.okapi.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author tdolega
+ */
+public class MessagesTest {
+
+  @Test
+  public void test1() {
+    Messages my = Messages.getInstance();
+    Assert.assertEquals("Invalid parameters:", my.getMessage("en", "10004"));
+    System.out.println("-----" + my.getMessage("en", "10004") + "-----");
+  }
+}
