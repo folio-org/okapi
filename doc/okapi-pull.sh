60854983a3aeb04371d771f67d085ba1f344874b
Adam Dickmeiss <adam@indexdata.dk>
2019-02-26 13:10:17 +0100

    Change defult URL for registry

--- doc/okapi-pull.sh
+++ doc/okapi-pull.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 O=http://localhost:9130
-U=${1:-http://folio-registry.aws.indexdata.com:9130}
+U=${1:-http://folio-registry.aws.indexdata.com:80}
 echo "{\"urls\" : [ \"$U\" ]}" >p.json
 curl -s -D - -XPOST -H "Content-Type: application/json" -d@p.json $O/_/proxy/pull/modules
 
