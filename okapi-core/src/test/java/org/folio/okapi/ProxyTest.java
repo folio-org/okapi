cdb90dbbee540421601a27dda9f4e7821faa5bc7
Adam Dickmeiss <adam@indexdata.dk>
2019-05-05 18:46:23 +0200

    Little more flexible in timer test

--- okapi-core/src/test/java/org/folio/okapi/ProxyTest.java
+++ okapi-core/src/test/java/org/folio/okapi/ProxyTest.java
@@ -2443,7 +2443,7 @@ public class ProxyTest {
       .then().statusCode(200).log().ifValidationFails();
 
     // 10 msecond period and 100 total wait time.. 1 tick per call.. So 8-10 calls
-    context.assertTrue(timerDelaySum >= 106 && timerDelaySum <= 110, "Got " + timerDelaySum);
+    context.assertTrue(timerDelaySum >= 104 && timerDelaySum <= 110, "Got " + timerDelaySum);
     logger.info("timerDelaySum=" + timerDelaySum);
   }
 }
