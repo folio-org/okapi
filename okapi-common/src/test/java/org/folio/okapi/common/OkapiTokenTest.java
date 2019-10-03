0ef1ab7370b937b1de680889c3aacfdd230739b0
Julian Ladisch <julianladisch@users.noreply.github.com>
2019-10-03 20:34:17 +0200

    Change Base64 encoded UUID accordingly.w

--- okapi-common/src/test/java/org/folio/okapi/common/OkapiTokenTest.java
+++ okapi-common/src/test/java/org/folio/okapi/common/OkapiTokenTest.java
@@ -25,7 +25,7 @@ public class OkapiTokenTest {
   @Test
   public void test2() {
     String s = "eyJzdWIiOiJfX3VuZGVmaW5lZF9fIiwidXNlcl9pZCI6Ijk5OTk5OTk5L"
-      + "Tk5OTktOTk5OS05OTk5LTk5OTk5OTk5OTk5OSIsInRlbmFudCI6InRlc3RsaWIifQ";
+      + "Tk5OTktNDk5OS05OTk5LTk5OTk5OTk5OTk5OSIsInRlbmFudCI6InRlc3RsaWIifQ";
     byte[] buf = Base64.getDecoder().decode(s);
     String got = new String(buf);
     String exp = "{\"sub\":\"__undefined__\","
