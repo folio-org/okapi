1415ec629aa5e2ea165e66ae533b567cfe28c357
Adam Dickmeiss <adam@indexdata.dk>
2019-12-10 12:55:58 +0100

    SemVer, ModuleId: JavaDoc, unit tests, code review OKAPI-781 (#862)
    Ensure transitive sorting property

--- okapi-common/src/main/java/org/folio/okapi/common/SemVer.java
+++ okapi-common/src/main/java/org/folio/okapi/common/SemVer.java
@@ -4,12 +4,24 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Semantic version implementation. Based
+ * <a href="https://semver.org/spec/v1.0.0.html">semver 1.0.0</a>,
+ * but is a little liberal at the moment, eg only major component
+ * is (eg 1) required or even more than 3 components for dot-separated
+ * list (eg 1.2.3.4).
+ */
 public class SemVer implements Comparable<SemVer> {
 
   private final List<String> preRelease = new LinkedList<>();
   private final List<String> versions = new LinkedList<>();
-  private String metadata;
+  private final String metadata;
 
+  /**
+   * Construct semantic version from string
+   * May throw IllegalArgumentException if string supplied can not be parsed
+   * @param v semantic version
+   */
   public SemVer(String v) {
     int offset = 0;
 
@@ -44,11 +56,40 @@ public class SemVer implements Comparable<SemVer> {
     }
   }
 
+  private boolean allDigits(String s) {
+    for (int i = 0; i < s.length(); i++) {
+      if (!Character.isDigit(s.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Compare two version components
+   * If both operands are numeric, compare numerically
+   * If both operands are non-numeric, compare lexicographically
+   * If mixed, non-numeric operand compares over numeric operand
+   * No comparison for mixed operands - in order offer transitive sorting
+   * In reality not a problem because systems most likely will use a common
+   * scheme for each component for some versioning system
+   * @param c1 left operand
+   * @param c2 right operand
+   * @return <0 if c1 < c2, 0 if c1 == c2, >0 if c1 > c2
+   */
   private int compareComp(String c1, String c2) {
-    if (Character.isDigit(c1.charAt(0)) && Character.isDigit(c2.charAt(0))) {
-      return Integer.parseInt(c1) - Integer.parseInt(c2);
+    if (allDigits(c1)) {
+      if (allDigits(c2)) {
+        return Integer.parseInt(c1) - Integer.parseInt(c2);
+      } else {
+        return -1;
+      }
     } else {
-      return c1.compareTo(c2);
+      if (allDigits(c2)) {
+        return 1;
+      } else {
+        return c1.compareTo(c2);
+      }
     }
   }
 
@@ -57,7 +98,7 @@ public class SemVer implements Comparable<SemVer> {
     if (!digits) {
       while (i < v.length()) {
         char ch = v.charAt(i);
-        if (ch == '-' || ch == '+' || ch == '.') {
+        if (ch == '+' || ch == '.') {
           break;
         }
         i++;
@@ -74,14 +115,27 @@ public class SemVer implements Comparable<SemVer> {
     return i;
   }
 
+  /**
+   * Whether the version appears to be an NPM snapshot (at least in FOLIO)
+   * @return true if it appears to be an NPM snapshot; false otherwise
+   */
   public boolean hasNpmSnapshot() {
     return versions.size() == 3 && versions.get(2).length() >= 5;
   }
 
+  /**
+   * Whether version has pre-release
+   * @return true if version has pre-release; false otherwise
+   */
   public boolean hasPreRelease() {
     return !preRelease.isEmpty();
   }
 
+  /**
+   * Whether this version has prefix of other
+   * @param other the prefix
+   * @return true if this version has prefix of other; false otherwise
+   */
   public boolean hasPrefix(SemVer other) {
     Iterator<String> i1 = this.versions.iterator();
     Iterator<String> i2 = other.versions.iterator();
@@ -91,11 +145,11 @@ public class SemVer implements Comparable<SemVer> {
         return false;
       }
     }
-    if (!i1.hasNext() && i2.hasNext()) {
-      return false;
-    }
-    else if (i1.hasNext() && !i2.hasNext()) {
+    if (i1.hasNext()) { // i2.hasNext is null
       return true;
+    }
+    if (i2.hasNext()) { // i1.hasNext is null
+      return false;
     }
     i1 = this.preRelease.iterator();
     i2 = other.preRelease.iterator();
@@ -105,23 +159,32 @@ public class SemVer implements Comparable<SemVer> {
         return false;
       }
     }
-    if (!i1.hasNext() && i2.hasNext()) {
-      return false;
-    }
-    else if (i1.hasNext() && !i2.hasNext()) {
+    if (i1.hasNext()) {
       return true;
     }
-    if (this.metadata != null && other.metadata != null) {
-      int v = this.metadata.compareTo(other.metadata);
-      if (v != 0) {
+    if (i2.hasNext()) {
+      return false;
+    }
+    if (other.metadata != null) {
+      if (this.metadata != null) {
+        int v = this.metadata.compareTo(other.metadata);
+        if (v != 0) {
+          return false;
+        }
+      } else {
         return false;
       }
-    } else if (this.metadata == null && other.metadata != null) {
-      return false;
     }
     return true;
   }
 
+  /**
+   * Compares two versions
+   * @param other
+   * @return -4 is this is major less than other; -3 if this is minor less than
+   * other; -2 if this is patch less than other; -1 for pre-release/build less;
+   * 0 if version as equal; 1, 2, 3, 4, as the opposite negatives
+   */
   @Override
   public int compareTo(SemVer other) {
     Iterator<String> i1 = this.versions.iterator();
@@ -151,7 +214,7 @@ public class SemVer implements Comparable<SemVer> {
     }
     if (i1.hasNext() && !i2.hasNext()) {
       return -1;
-      }
+    }
     while (i1.hasNext() && i2.hasNext()) {
       int v = compareComp(i1.next(), i2.next());
       if (v > 0) {
@@ -162,24 +225,32 @@ public class SemVer implements Comparable<SemVer> {
     }
     if (i1.hasNext()) {
       return 1;
-    } else if (i2.hasNext()) {
+    }
+    if (i2.hasNext()) {
       return -1;
     }
-    if (this.metadata != null && other.metadata != null) {
-      int v = this.metadata.compareTo(other.metadata);
-      if (v > 0) {
+    if (this.metadata != null) {
+      if (other.metadata != null) {
+        int v = this.metadata.compareTo(other.metadata);
+        if (v > 0) {
+          return 1;
+        } else if (v < 0) {
+          return -1;
+        }
+      } else {
         return 1;
-      } else if (v < 0) {
-        return -1;
       }
-    } else if (this.metadata != null && other.metadata == null) {
-      return 1;
-    } else if (this.metadata == null && other.metadata != null) {
+    } else if (other.metadata != null) {
       return -1;
     }
     return 0;
   }
 
+  /**
+   * Whether this version is equal to other
+   * @param that
+   * @return true if equal; false otherwise
+   */
   @Override
   public boolean equals(Object that) {
     if (this == that) {
@@ -210,12 +281,10 @@ public class SemVer implements Comparable<SemVer> {
     StringBuilder b = new StringBuilder();
     String sep = "";
     Iterator<String> it = this.versions.iterator();
-    if (it.hasNext()) {
-      while (it.hasNext()) {
-        b.append(sep);
-        b.append(it.next());
-        sep = ".";
-      }
+    while (it.hasNext()) {
+      b.append(sep);
+      b.append(it.next());
+      sep = ".";
     }
     sep = "-";
     it = this.preRelease.iterator();
