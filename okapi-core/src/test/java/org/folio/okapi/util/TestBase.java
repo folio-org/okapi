f95f1db9300fa241aa6a45d6339a5474d80c8619
Julian Ladisch <julianladisch@users.noreply.github.com>
2020-06-10 19:10:38 +0200

    OKAPI-787: Support SSL connections to Postgres
    Add new configuration option postgres_server_pem.
    
    Add netty-tcnative-boringssl-static because OpenJDK 8
    doesn't support TLSv1.3.
    
    For testing a postgres:12-alpine testingcontainers.org
    container is startet. PostgreSQL 10 does not support
    ssl_min_protocol_version that we need for the TLSv1.3
    test. The Alpine version is sufficient, we don't need
    the large Debian container.
    
    This also introduces JUnit 5 (Jupiter).

--- okapi-core/src/test/java/org/folio/okapi/util/TestBase.java
+++ okapi-core/src/test/java/org/folio/okapi/util/TestBase.java
@@ -3,6 +3,11 @@ package org.folio.okapi.util;
 import io.vertx.core.Handler;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import org.folio.okapi.common.ExtendedAsyncResult;
 
 public class TestBase {
@@ -33,5 +38,26 @@ public class TestBase {
       context.verify(block);
       async.complete();
     };
+  }
+
+  /**
+   * @return resource file at resourcePath as UTF-8
+   */
+  protected static String getResource(String resourcePath) {
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    try (InputStream inputStream = classLoader.getResourceAsStream(resourcePath)) {
+      ByteArrayOutputStream result = new ByteArrayOutputStream();
+      byte [] buffer = new byte[1024];
+      while (true) {
+        int length = inputStream.read(buffer);
+        if (length == -1) {
+          break;
+        }
+        result.write(buffer, 0, length);
+      }
+      return result.toString(StandardCharsets.UTF_8.name());
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
   }
 }
