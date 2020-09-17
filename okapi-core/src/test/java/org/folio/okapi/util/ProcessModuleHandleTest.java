b5fac4836d287f0e1e02414673faa2c10885b19b
Adam Dickmeiss <adam@indexdata.dk>
2020-09-17 13:48:00 +0200

    Use context.asyncAssert stuff rather than async

--- okapi-core/src/test/java/org/folio/okapi/util/ProcessModuleHandleTest.java
+++ okapi-core/src/test/java/org/folio/okapi/util/ProcessModuleHandleTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.net.NetServer;
-import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.apache.logging.log4j.Logger;
@@ -43,174 +42,115 @@ public class ProcessModuleHandleTest {
   }
 
   @Test
-  public void test1(TestContext context) {
-    final Async async = context.async();
+  public void testProgramNoPort(TestContext context) {
     LaunchDescriptor desc = new LaunchDescriptor();
     // program starts OK, we don't check port
     desc.setExec("java -version %p");
     ModuleHandle mh = createModuleHandle(desc, 0);
 
-    mh.start(res -> {
-      if (!res.succeeded()) {
-        logger.error("CAUSE: " + res.cause());
-      }
-      context.assertTrue(res.succeeded());
-      if (!res.succeeded()) {
-        async.complete();
-        return;
-      }
-      mh.stop(res2 -> {
-        context.assertTrue(res2.succeeded());
-        async.complete();
-      });
-    });
+    mh.start(context.asyncAssertSuccess(res1 ->
+      mh.stop(context.asyncAssertSuccess())
+    ));
   }
 
   @Test
-  public void test1a(TestContext context) {
-    final Async async = context.async();
+  public void testProgramNoListener(TestContext context) {
     LaunchDescriptor desc = new LaunchDescriptor();
     // program starts OK, but do not listen to port..
     desc.setExec("java -version %p");
     ModuleHandle mh = createModuleHandle(desc, 9231);
 
-    mh.start(res -> {
-      if (res.succeeded()) { // error did not expect to succeed!
-        mh.stop(res2 -> {
-          context.assertTrue(res2.succeeded());
-          context.assertFalse(res.failed());
-          async.complete();
-        });
-      }
-      context.assertFalse(res.succeeded());
-      String msg = res.cause().getMessage();
-      context.assertTrue(msg.startsWith("Deployment failed. Could not connect to port 9231"));
-      async.complete();
-    });
+    mh.start(context.asyncAssertFailure(cause ->
+      context.assertTrue(cause.getMessage().startsWith("Deployment failed. Could not connect to port 9231"))
+    ));
   }
 
-
   @Test
-  public void test2(TestContext context) {
-    final Async async = context.async();
+  public void testProgramDoesNotExist(TestContext context) {
     LaunchDescriptor desc = new LaunchDescriptor();
     // program does not exist (we hope)
     desc.setExec("gyf 10 %p"); // bad program
     ModuleHandle mh = createModuleHandle(desc, 0);
 
-    mh.start(res -> {
-      context.assertFalse(res.succeeded());
-      async.complete();
-    });
+    mh.start(context.asyncAssertFailure());
   }
 
   @Test
-  public void test3(TestContext context) {
-    final Async async = context.async();
+  public void testProgramReturnsError(TestContext context) {
     LaunchDescriptor desc = new LaunchDescriptor();
     // program returns immediately with exit code
     desc.setExec("java -Dport=%p -jar unknown.jar");
     ModuleHandle mh = createModuleHandle(desc, 9231);
 
-    mh.start(res -> {
-      context.assertFalse(res.succeeded());
-      async.complete();
-    });
+    mh.start(context.asyncAssertFailure());
   }
 
   @Test
   public void testNoExecNoCmdLineStart(TestContext context) {
-    final Async async = context.async();
     LaunchDescriptor desc = new LaunchDescriptor();
     // no cmdlineStart, no exec
     ModuleHandle mh = createModuleHandle(desc, 9231);
 
-    mh.start(res -> {
-      context.assertTrue(res.failed());
-      context.assertEquals("Can not deploy: No exec, no CmdlineStart in LaunchDescriptor", res.cause().getMessage());
-      async.complete();
-    });
+    mh.start(context.asyncAssertFailure(cause ->
+      context.assertEquals("Can not deploy: No exec, no CmdlineStart in LaunchDescriptor", cause.getMessage())
+    ));
   }
 
   @Test
   public void testMissingPercentPexec(TestContext context) {
-    final Async async = context.async();
     LaunchDescriptor desc = new LaunchDescriptor();
     desc.setExec("java -Dport=9000 -jar unknown.jar");
     ModuleHandle mh = createModuleHandle(desc, 9231);
 
-    mh.start(res -> {
-      context.assertFalse(res.succeeded());
-      context.assertEquals("Can not deploy: No %p in the exec line", res.cause().getMessage());
-      async.complete();
-    });
+    mh.start(context.asyncAssertFailure(cause ->
+      context.assertEquals("Can not deploy: No %p in the exec line", cause.getMessage())
+    ));
   }
 
   @Test
   public void testMissingPercentPcmdlineStart(TestContext context) {
-    final Async async = context.async();
     LaunchDescriptor desc = new LaunchDescriptor();
     desc.setCmdlineStart("java -Dport=9000 -jar unknown.jar");
     ModuleHandle mh = createModuleHandle(desc, 9231);
 
-    mh.start(res -> {
-      context.assertFalse(res.succeeded());
-      context.assertEquals("Can not deploy: No %p in the cmdlineStart", res.cause().getMessage());
-      async.complete();
-    });
+    mh.start(context.asyncAssertFailure(cause ->
+      context.assertEquals("Can not deploy: No %p in the cmdlineStart", cause.getMessage())
+    ));
   }
 
   @Test
   public void testExecOk(TestContext context) {
-    final Async async = context.async();
     LaunchDescriptor desc = new LaunchDescriptor();
     // program should operate OK
     desc.setExec("java " + testModuleArgs);
     ModuleHandle mh = createModuleHandle(desc, 9231);
 
-    mh.start(res1 -> {
-      mh.stop(res2 -> {
-        context.assertTrue(res2.succeeded());
-        async.complete();
-      });
-    });
+    mh.start(context.asyncAssertSuccess(res ->
+      mh.stop(context.asyncAssertSuccess())
+    ));
   }
 
   @Test
   public void testPortAlreadyInUse(TestContext context) {
-    final Async async = context.async();
-
     final NetServer ns = vertx.createNetServer().connectHandler( res -> { res.close(); });
-    ns.listen(9231, res -> {
-      if (res.failed()) {
-        async.complete();
+    ns.listen(9231, context.asyncAssertSuccess(res -> {
+      LaunchDescriptor desc = new LaunchDescriptor();
+      desc.setExec("java " + testModuleArgs);
+      ModuleHandle mh = createModuleHandle(desc, 9231);
+      mh.start(context.asyncAssertFailure(cause -> {
+        context.assertEquals("port 9231 already in use", cause.getMessage());
         ns.close();
-      } else {
-        LaunchDescriptor desc = new LaunchDescriptor();
-        desc.setExec("java " + testModuleArgs);
-        ModuleHandle mh = createModuleHandle(desc, 9231);
-        mh.start(res1 -> {
-          context.assertTrue(res1.failed());
-          context.assertEquals("port 9231 already in use", res1.cause().getMessage());
-          ns.close();
-          // stop is not necessary, but check that we can call it anyway
-          mh.stop(res2 -> {
-            context.assertTrue(res2.succeeded());
-            async.complete();
-          });
-        });
-      }
-    });
-
+        // stop is not necessary, but check that we can call it anyway
+        mh.stop(context.asyncAssertSuccess());
+      }));
+    }));
   }
 
   @Test
   public void testCmdLineOk(TestContext context) {
-    final Async async = context.async();
     // Cannot rely on sh and kill on Windows
     String os = System.getProperty("os.name").toLowerCase();
     if (os.contains("win")) {
-      async.complete();
       return;
     }
     LaunchDescriptor desc = new LaunchDescriptor();
@@ -219,22 +159,14 @@ public class ProcessModuleHandleTest {
     desc.setCmdlineStop("kill `cat test-module.pid`; rm -f test-module.pid");
     ModuleHandle mh = createModuleHandle(desc, 9231);
 
-    mh.start(res1 -> {
-      context.assertTrue(res1.succeeded());
-      mh.stop(res2 -> {
-        context.assertTrue(res2.succeeded());
-        async.complete();
-      });
-    });
+    mh.start(context.asyncAssertSuccess(res -> mh.stop(context.asyncAssertSuccess())));
   }
 
   @Test
   public void testBadCmdlineStop(TestContext context) {
-    final Async async = context.async();
     // Cannot rely on sh and kill on Windows
     String os = System.getProperty("os.name").toLowerCase();
     if (os.contains("win")) {
-      async.complete();
       return;
     }
     LaunchDescriptor desc = new LaunchDescriptor();
@@ -243,22 +175,14 @@ public class ProcessModuleHandleTest {
     desc.setCmdlineStop("gyf");
     ModuleHandle mh = createModuleHandle(desc, 0);
 
-    mh.start(res1 -> {
-      context.assertTrue(res1.succeeded());
-      mh.stop(res2 -> {
-        context.assertTrue(res2.failed());
-        async.complete();
-      });
-    });
+    mh.start(context.asyncAssertSuccess(res -> mh.stop(context.asyncAssertFailure())));
   }
 
   @Test
-  public void testNoListenCmdlineStart(TestContext context) {
-    final Async async = context.async();
+  public void testCmdlineStartNoListener(TestContext context) {
     // Cannot rely on sh and kill on Windows
     String os = System.getProperty("os.name").toLowerCase();
     if (os.contains("win")) {
-      async.complete();
       return;
     }
     LaunchDescriptor desc = new LaunchDescriptor();
@@ -267,19 +191,14 @@ public class ProcessModuleHandleTest {
     desc.setCmdlineStop("gyf");
     ModuleHandle mh = createModuleHandle(desc, 9231);
 
-    mh.start(res -> {
-      context.assertTrue(res.failed());
-      async.complete();
-    });
+    mh.start(context.asyncAssertFailure());
   }
 
   @Test
   public void testCmdlineStartFails(TestContext context) {
-    final Async async = context.async();
     // Cannot rely on sh and kill on Windows
     String os = System.getProperty("os.name").toLowerCase();
     if (os.contains("win")) {
-      async.complete();
       return;
     }
     LaunchDescriptor desc = new LaunchDescriptor();
@@ -288,11 +207,7 @@ public class ProcessModuleHandleTest {
     desc.setCmdlineStop("gyf");
     ModuleHandle mh = createModuleHandle(desc, 0);
 
-    mh.start(res -> {
-      context.assertTrue(res.failed());
-      async.complete();
-    });
-
+    mh.start(context.asyncAssertFailure());
   }
 
 }
