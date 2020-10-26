cfb0db0ed235b51a5fff31e53906c5d15e6cae4e
Adam Dickmeiss <adam@indexdata.dk>
2020-10-26 12:43:13 +0100

    Unused constructor

--- okapi-core/src/main/java/org/folio/okapi/bean/Permission.java
+++ okapi-core/src/main/java/org/folio/okapi/bean/Permission.java
@@ -16,18 +16,6 @@ public class Permission {
   public Permission() {
   }
 
-  /**
-   * Create permission from existing permission.
-   * @param other existing permission
-   */
-  public Permission(Permission other) {
-    this.permissionName = other.permissionName;
-    this.displayName = other.displayName;
-    this.description = other.description;
-    this.subPermissions = other.subPermissions;
-    this.visible = other.visible;
-  }
-
   public String getPermissionName() {
     return permissionName;
   }
