a10a105ab55b5afc5ff07aaf25aa3d5710791b63
Adam Dickmeiss <adam@indexdata.dk>
2020-11-04 15:24:51 +0100

    Simplify instance list assertions

--- okapi-core/src/test/java/org/folio/okapi/util/ModuleCacheTest.java
+++ okapi-core/src/test/java/org/folio/okapi/util/ModuleCacheTest.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import org.folio.okapi.bean.InterfaceDescriptor;
 import org.folio.okapi.bean.ModuleDescriptor;
 import org.folio.okapi.bean.ModuleInstance;
@@ -12,12 +13,13 @@ import org.folio.okapi.bean.RoutingEntry;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ModuleCacheTest {
+  Function<ModuleInstance, RoutingEntry> routingEntry = ModuleInstance::getRoutingEntry;
+
   @ParameterizedTest
   @CsvSource({
       "/, /",
@@ -91,52 +93,44 @@ class ModuleCacheTest {
     assertThat(ModuleCache.lookup("/", HttpMethod.GET, map, true, null)).isEmpty();
     assertThat(ModuleCache.lookup("/a", HttpMethod.GET, map, true, null)).isEmpty();
     List<ModuleInstance> instances = ModuleCache.lookup("/a/b", HttpMethod.GET, map, true, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry1);
+
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry1);
 
     instances = ModuleCache.lookup("/a/b", HttpMethod.GET, map, true, "module-1.0.0");
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry1);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry1);
 
     assertThat(ModuleCache.lookup("/a", HttpMethod.GET, map, true, "other-1.0.0")).isEmpty();
 
     instances = ModuleCache.lookup("/a/b", HttpMethod.POST, map, true, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry2);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry2);
 
     assertThat(ModuleCache.lookup("/a", HttpMethod.PUT, map, true, null)).isEmpty();
     assertThat(ModuleCache.lookup("/a/b/", HttpMethod.GET, map, true, null)).isEmpty();
 
     instances = ModuleCache.lookup("/a/b/id/c", HttpMethod.GET, map, true, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry3);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry3);
 
     instances = ModuleCache.lookup("/p/id/y", HttpMethod.GET, map, true, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry4);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry4);
 
     ModuleCache.lookup("/p/id/z", HttpMethod.GET, map, true, null).isEmpty();
     ModuleCache.lookup("/p/id", HttpMethod.GET, map, true, null).isEmpty();
     ModuleCache.lookup("/p/id/y/z", HttpMethod.GET, map, true, null).isEmpty();
 
     instances = ModuleCache.lookup("/perms/users", HttpMethod.GET, map, true, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry5);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry5);
 
     instances = ModuleCache.lookup("/perms/users/y", HttpMethod.GET, map, true, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry5);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry5);
 
     instances = ModuleCache.lookup("/perms/users1", HttpMethod.GET, map, true, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry5);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry5);
 
     ModuleCache.lookup("/perms/user", HttpMethod.GET, map, true, null).isEmpty();
 
     assertThat(ModuleCache.lookup("/old/foo", HttpMethod.GET, map, true, null)).isEmpty();
     instances = ModuleCache.lookup("/old/type", HttpMethod.GET, map, true, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry6);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry6);
   }
 
   @Test
@@ -169,26 +163,23 @@ class ModuleCacheTest {
     assertThat(moduleCache.getModules()).isEqualTo(modules);
 
     List<ModuleInstance> instances = moduleCache.lookup("/a/id", HttpMethod.GET, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry2);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry2);
 
     instances = moduleCache.lookup("/a/id#a", HttpMethod.GET, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry2);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry2);
 
     instances = moduleCache.lookup("/a/id?a", HttpMethod.GET, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry2);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry2);
 
     assertThat(moduleCache.lookup("/a/id", HttpMethod.GET, "module-id")).isEmpty();
 
     instances = moduleCache.lookup("/a/client_id", HttpMethod.GET, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry1);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry1);
   }
 
   @Test
   void testModuleMulti() {
+
     RoutingEntry[] routingEntries = new RoutingEntry[1];
     RoutingEntry routingEntry1 = routingEntries[0] = new RoutingEntry();
     routingEntry1.setPathPattern("/a/{id}");
@@ -209,8 +200,7 @@ class ModuleCacheTest {
     ModuleCache moduleCache = new ModuleCache(modules);
 
     List<ModuleInstance> instances = moduleCache.lookup("/a/id", HttpMethod.GET, "module-1.0.0");
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry1);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry1);
 
     assertThat(moduleCache.lookup("/a/id", HttpMethod.GET, "other-1.0.0")).isEmpty();
     assertThat(moduleCache.lookup("/a/id", HttpMethod.GET, null)).isEmpty();
@@ -244,17 +234,13 @@ class ModuleCacheTest {
     ModuleCache moduleCache = new ModuleCache(modules);
 
     List<ModuleInstance> instances = moduleCache.lookup("/a/id", HttpMethod.GET, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry2);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry2);
 
     instances = moduleCache.lookup("/token", HttpMethod.POST, null);
-    assertThat(instances.size()).isEqualTo(1);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry2);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry2);
 
     instances = moduleCache.lookup("/token", HttpMethod.GET, null);
-    assertThat(instances.size()).isEqualTo(2);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry2);
-    assertThat(instances.get(1).getRoutingEntry()).isEqualTo(routingEntry1);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry2, routingEntry1);
   }
 
   @Test
@@ -331,15 +317,10 @@ class ModuleCacheTest {
     ModuleCache moduleCache2 = new ModuleCache(modules);
 
     List<ModuleInstance> instances = moduleCache2.lookup("/second", HttpMethod.GET, null);
-    assertThat(instances.size()).isEqualTo(2);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry2);
-    assertThat(instances.get(1).getRoutingEntry()).isEqualTo(routingEntry1);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry2, routingEntry1);
 
     instances = moduleCache2.lookup("/third", HttpMethod.GET, null);
-    assertThat(instances.size()).isEqualTo(3);
-    assertThat(instances.get(0).getRoutingEntry()).isEqualTo(routingEntry3);
-    assertThat(instances.get(1).getRoutingEntry()).isEqualTo(routingEntry2);
-    assertThat(instances.get(2).getRoutingEntry()).isEqualTo(routingEntry1);
+    assertThat(instances).extracting(routingEntry).containsExactly(routingEntry3, routingEntry2, routingEntry1);
   }
 
 
