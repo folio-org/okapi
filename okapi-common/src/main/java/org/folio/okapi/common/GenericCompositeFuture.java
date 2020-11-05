a31e0745a7afc57a8db91a8c97c34c093739abe2
Julian Ladisch <julianladisch@users.noreply.github.com>
2020-11-05 21:49:09 +0100

    Change javadoc paragraphs from Vert.x to Google Code Style

--- okapi-common/src/main/java/org/folio/okapi/common/GenericCompositeFuture.java
+++ okapi-common/src/main/java/org/folio/okapi/common/GenericCompositeFuture.java
@@ -12,8 +12,8 @@ import java.util.List;
  * {@link CompositeFuture#all(List)}, {@link CompositeFuture#any(List)} and
  * {@link CompositeFuture#join(List)} but properly check the generic type
  * {@code <T extends Future<?>>}.
- * <p>
- * See <a href="https://github.com/eclipse-vertx/vert.x/pull/3595">
+ *
+ * <p>See <a href="https://github.com/eclipse-vertx/vert.x/pull/3595">
  * https://github.com/eclipse-vertx/vert.x/pull/3595</a>
  * why Vert.x doesn't have this.
  */
@@ -21,10 +21,10 @@ public interface GenericCompositeFuture {
   /**
    * Return a composite future, succeeded when all futures are succeeded,
    * failed as soon as one of the futures is failed.
-   * <p/>
-   * When the list is empty, the returned future will be already succeeded.
-   * <p/>
-   * This is the same as {@link CompositeFuture#all(List)} but with generic type declaration.
+   *
+   * <p>When the list is empty, the returned future will be already succeeded.
+   *
+   * <p>This is the same as {@link CompositeFuture#all(List)} but with generic type declaration.
    */
   static <T extends Future<?>> CompositeFuture all(List<T> futures) {
     return CompositeFutureImpl.all(futures.toArray(new Future[futures.size()]));
@@ -33,10 +33,10 @@ public interface GenericCompositeFuture {
   /**
    * Return a composite future, succeeded as soon as one future is succeeded,
    * failed when all futures are failed.
-   * <p/>
-   * When the list is empty, the returned future will be already succeeded.
-   * <p/>
-   * This is the same as {@link CompositeFuture#any(List)} but with generic type declaration.
+   *
+   * <p>When the list is empty, the returned future will be already succeeded.
+   *
+   * <p>This is the same as {@link CompositeFuture#any(List)} but with generic type declaration.
    */
   static <T extends Future<?>> CompositeFuture any(List<T> futures) {
     return CompositeFutureImpl.any(futures.toArray(new Future[futures.size()]));
@@ -45,13 +45,13 @@ public interface GenericCompositeFuture {
   /**
    * Return a composite future, succeeded when all {@code futures} are succeeded,
    * failed when any of the {@code futures} is failed.
-   * <p/>
-   * It always waits until all its {@code futures} are completed and will not fail
+   *
+   * <p>It always waits until all its {@code futures} are completed and will not fail
    * as soon as one of the {@code futures} fails.
-   * <p/>
-   * When the list is empty, the returned future will be already succeeded.
-   * <p/>
-   * This is the same as {@link CompositeFuture#join(List)} but with generic type declaration.
+   *
+   * <p>When the list is empty, the returned future will be already succeeded.
+   *
+   * <p>This is the same as {@link CompositeFuture#join(List)} but with generic type declaration.
    */
   static <T extends Future<?>> CompositeFuture join(List<T> futures) {
     return CompositeFutureImpl.join(futures.toArray(new Future[futures.size()]));
