0539863d47b5d9bd912a44e0d32c2bf609561c97
Hongwei Ji <hji@ebsco.com>
2020-11-09 05:40:32 -0500

    use assertThat

--- okapi-core/src/test/java/org/folio/okapi/service/impl/DockerModuleHandleTest.java
+++ okapi-core/src/test/java/org/folio/okapi/service/impl/DockerModuleHandleTest.java
@@ -511,8 +511,7 @@ public class DockerModuleHandleTest implements WithAssertions {
     {
       Async async = context.async();
       dh.pullImage().onComplete(context.asyncAssertFailure(res -> {
-        context.assertTrue(res.getMessage().contains("9231")
-            && res.getMessage().contains("connection refused"), res.getMessage());
+        assertThat(res.getMessage()).contains("9231").contains("connection refused");
         async.complete();
       }));
       async.await();
