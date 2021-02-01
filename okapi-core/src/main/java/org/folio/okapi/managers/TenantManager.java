259183e7fdb23501c58e9a40d60082493e5d9129
Adam Dickmeiss <adam@indexdata.dk>
2021-02-01 20:09:13 +0100

    Ensure module is logged for tenant operation failures (#1076)

--- okapi-core/src/main/java/org/folio/okapi/managers/TenantManager.java
+++ okapi-core/src/main/java/org/folio/okapi/managers/TenantManager.java
@@ -318,7 +318,8 @@ public class TenantManager implements Liveness {
                       message.append(ar.getString(i));
                     }
                   }
-                  promise.fail(message.toString());
+                  promise.fail(messages.getMessage("10410",
+                      getInstance.getModuleDescriptor().getId(), message.toString()));
                 });
             return;
           }
