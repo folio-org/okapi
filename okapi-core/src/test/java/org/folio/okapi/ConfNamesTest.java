8e32b1af82ed4cbaf9cb7e94b52a48fbf52a5ed7
Julian Ladisch <julianladisch@users.noreply.github.com>
2021-03-03 16:04:50 +0100

    Use UtilityClassTester from folio.org

---
+++ okapi-core/src/test/java/org/folio/okapi/ConfNamesTest.java
@@ -0,0 +1,12 @@
+package org.folio.okapi;
+
+import org.folio.rest.testing.UtilityClassTester;
+import org.junit.jupiter.api.Test;
+
+public class ConfNamesTest {
+
+  @Test
+  void utilityClass() {
+    UtilityClassTester.assertUtilityClass(ConfNames.class);
+  }
+}
