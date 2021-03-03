9ea931317c4bb356076d35bd0afbc5fca5405742
Julian Ladisch <julianladisch@users.noreply.github.com>
2021-03-03 15:13:20 +0100

    Extract configuration variable names into ConfNames.java

---
+++ okapi-core/src/main/java/org/folio/okapi/ConfNames.java
@@ -0,0 +1,21 @@
+package org.folio.okapi;
+
+import org.folio.okapi.common.Config;
+
+/**
+ * Configuration variable names used for {@code java -Dname=value okapi.jar}
+ * or used as key in okapi-conf.json when calling {@code java okapi.jar -conf okapi-conf.json}.
+ *
+ * @see Config
+ * @see <a href="https://github.com/folio-org/okapi/blob/master/doc/guide.md#okapi-configuration">
+ *     guide.md#okapi-configuration</a>
+ */
+public final class ConfNames {
+
+  public static final String DEPLOY_WAIT_ITERATIONS = "deploy_waitIterations";
+  public static final String DOCKER_URL = "dockerUrl";
+
+  private ConfNames() {
+    throw new UnsupportedOperationException("Cannot instantiate utility class.");
+  }
+}
