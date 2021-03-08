d67ab2bf564eb11f5292d66708a87b7c16ec610e
Adam Dickmeiss <adam@indexdata.dk>
2021-03-08 18:15:36 +0100

    Reload permissions immediately on enable (not upgrade) OKAPI-996
    1. reload permissions when mod-permissions is being enabled (not upgraded)
    2. reload permissions at end of install when mod-permissions is upgraded.
    
    Previously permissions was reloaded at the end always.

--- okapi-core/src/test/java/org/folio/okapi/ModuleTest.java
+++ okapi-core/src/test/java/org/folio/okapi/ModuleTest.java
@@ -2698,11 +2698,10 @@ public class ModuleTest {
           .get("/permResult")
           .then()
           .statusCode(200)
-          .body("$", hasSize(4))
-          .body("[0].moduleId", is("header-1"))
-          .body("[1].moduleId", is("header-1"))
-          .body("[2].moduleId", is("okapi-0.0.0"))
-          .body("[3].moduleId", is("sample-module-1.0.0"));
+          .body("$", hasSize(3))
+          .body("[0].moduleId", is("okapi-0.0.0"))
+          .body("[1].moduleId", is("sample-module-1.0.0"))
+          .body("[2].moduleId", is("header-1"));
     }
 
     conf.put("okapiVersion", "3.0.0");  // upgrade from 0.0.0 to 3.0.0
