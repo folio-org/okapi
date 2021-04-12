2f8271a28f2b6b409c9803337656d031f7cbd9fe
Hongwei Ji <hji@ebsco.com>
2021-04-12 10:02:28 -0400

    Simplify CORS delegate and support preflight too

--- okapi-core/src/main/java/org/folio/okapi/util/CorsHelper.java
+++ okapi-core/src/main/java/org/folio/okapi/util/CorsHelper.java
@@ -3,11 +3,12 @@ package org.folio.okapi.util;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.CorsHandler;
 import java.util.List;
 import org.folio.okapi.bean.ModuleInstance;
+import org.folio.okapi.common.HttpResponse;
 import org.folio.okapi.common.XOkapiHeaders;
+import org.folio.okapi.managers.TenantManager;
 
 /**
  * Customized CORS handling.
@@ -18,8 +19,8 @@ import org.folio.okapi.common.XOkapiHeaders;
  */
 public class CorsHelper {
 
-  private static final String CHECK_DELEGATE_CORS = "check-delegate-CORS";
-  private static final String DELEGATE_CORS = "delegate-CORS";
+  public static final String DELEGATE_CORS = "delegate-CORS";
+  public static final String DELEGATE_CORS_MODULE_INSTANCE = "delegate-CORS-module-instance";
 
   private CorsHelper() {
   }
@@ -29,57 +30,60 @@ public class CorsHelper {
    *
    * @param router - {@link Router}
    */
-  public static void addCorsHandler(Router router) {
+  public static void addCorsHandler(Router router, final TenantManager tenantManager) {
 
+    // set delegate CORS for special cases
     router.routeWithRegex("^/_/invoke/tenant/[^/ ]+/.*").handler(ctx -> {
-      ctx.data().put(CHECK_DELEGATE_CORS, true);
-      ctx.next();
+      String origPath = ctx.request().path();
+      String tenantId = origPath.replaceFirst("^/_/invoke/tenant/([^/ ]+)/.*$", "$1");
+      String newPath = origPath.replaceFirst("^/_/invoke/tenant/[^/ ]+(/.*$)", "$1");
+      String meth = ctx.request().getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD);
+      HttpMethod method = meth != null ? HttpMethod.valueOf(meth) : ctx.request().method();
+      String moduleId = ctx.request().getHeader(XOkapiHeaders.MODULE_ID);
+      tenantManager.get(tenantId)
+          .onFailure(cause -> HttpResponse.responseError(ctx, 400, cause.getMessage()))
+          .onSuccess(tenant -> {
+            tenantManager.getModuleCache(tenant)
+                .onFailure(cause -> HttpResponse.responseError(ctx, 500, cause.getMessage()))
+                .onSuccess(moduleCache -> {
+                  List<ModuleInstance> list = moduleCache.lookup(newPath, method, moduleId);
+                  for (ModuleInstance mi : list) {
+                    if (mi.isHandler() && mi.getRoutingEntry().isDelegateCors()) {
+                      ctx.data().put(DELEGATE_CORS, true);
+                      ctx.data().put(DELEGATE_CORS_MODULE_INSTANCE, mi);
+                      break;
+                    }
+                  }
+                  ctx.next();
+                });
+          });
     });
 
+    // check delegate CORS
     router.route().handler(ctx -> {
       if (ctx.data().containsKey(DELEGATE_CORS)) {
         ctx.next();
       } else {
         CorsHandler.create("*")
-          .allowedMethod(HttpMethod.PUT)
-          .allowedMethod(HttpMethod.PATCH)
-          .allowedMethod(HttpMethod.DELETE)
-          .allowedMethod(HttpMethod.GET)
-          .allowedMethod(HttpMethod.POST)
-          .allowedHeader(HttpHeaders.CONTENT_TYPE.toString())
-          .allowedHeader(XOkapiHeaders.TENANT)
-          .allowedHeader(XOkapiHeaders.TOKEN)
-          .allowedHeader(XOkapiHeaders.AUTHORIZATION)
-          .allowedHeader(XOkapiHeaders.REQUEST_ID) //expose response headers
-          .allowedHeader(XOkapiHeaders.MODULE_ID)
-          .exposedHeader(HttpHeaders.LOCATION.toString())
-          .exposedHeader(XOkapiHeaders.TRACE)
-          .exposedHeader(XOkapiHeaders.TOKEN)
-          .exposedHeader(XOkapiHeaders.AUTHORIZATION)
-          .exposedHeader(XOkapiHeaders.REQUEST_ID)
-          .exposedHeader(XOkapiHeaders.MODULE_ID).handle(ctx);
+            .allowedMethod(HttpMethod.PUT)
+            .allowedMethod(HttpMethod.PATCH)
+            .allowedMethod(HttpMethod.DELETE)
+            .allowedMethod(HttpMethod.GET)
+            .allowedMethod(HttpMethod.POST)
+            .allowedHeader(HttpHeaders.CONTENT_TYPE.toString())
+            .allowedHeader(XOkapiHeaders.TENANT)
+            .allowedHeader(XOkapiHeaders.TOKEN)
+            .allowedHeader(XOkapiHeaders.AUTHORIZATION)
+            .allowedHeader(XOkapiHeaders.REQUEST_ID) // expose response headers
+            .allowedHeader(XOkapiHeaders.MODULE_ID)
+            .exposedHeader(HttpHeaders.LOCATION.toString())
+            .exposedHeader(XOkapiHeaders.TRACE)
+            .exposedHeader(XOkapiHeaders.TOKEN)
+            .exposedHeader(XOkapiHeaders.AUTHORIZATION)
+            .exposedHeader(XOkapiHeaders.REQUEST_ID)
+            .exposedHeader(XOkapiHeaders.MODULE_ID).handle(ctx);
       }
     });
-  }
-
-  /**
-   * Check CORS delegate to decide if reroute if necessary.
-   *
-   * @param ctx             - {@link RoutingContext}
-   * @param moduleInstances a list of {@link ModuleInstance}
-   * @return true if reroute if needed, false otherwise
-   */
-  public static boolean checkCorsDelegate(
-      RoutingContext ctx, List<ModuleInstance> moduleInstances) {
-    if (ctx.data().containsKey(CHECK_DELEGATE_CORS)) {
-      ctx.data().remove(CHECK_DELEGATE_CORS);
-      if (moduleInstances.stream().anyMatch(mi ->
-          mi.isHandler() && mi.getRoutingEntry().isDelegateCors())) {
-        ctx.data().put(DELEGATE_CORS, true);
-        return true;
-      }
-    }
-    return false;
   }
 
 }
