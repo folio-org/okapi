c9d791968d6798c53ccd2db7b0176bb26b6ab6ae
Julian Ladisch <julianladisch@users.noreply.github.com>
2021-12-16 14:15:04 +0100

    WebClientOptions, utility class test

--- okapi-common/src/main/java/org/folio/okapi/common/WebClientFactory.java
+++ okapi-common/src/main/java/org/folio/okapi/common/WebClientFactory.java
@@ -2,6 +2,7 @@ package org.folio.okapi.common;
 
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -17,9 +18,21 @@ public final class WebClientFactory {
 
   /**
    * Get a WebClient, returns the same instance for the same Vertx instance.
+   *
+   * <p>The webClientOptions parameter is only used when creating the WebClient,
+   * the options of an existing WebClient are not changed.
+   */
+  public static WebClient getWebClient(Vertx vertx, WebClientOptions webClientOptions) {
+    return clients.computeIfAbsent(vertx, x -> WebClient.create(vertx, webClientOptions));
+  }
+
+  /**
+   * Get a WebClient, returns the same instance for the same Vertx instance.
+   *
+   * <p>It doesn't reset WebClientOptions when returning an existing WebClient.
    */
   public static WebClient getWebClient(Vertx vertx) {
-    return clients.computeIfAbsent(vertx, x -> WebClient.create(vertx));
+    return getWebClient(vertx, new WebClientOptions());
   }
 }
 
--- okapi-common/src/test/java/org/folio/okapi/common/WebClientFactoryTest.java
+++ okapi-common/src/test/java/org/folio/okapi/common/WebClientFactoryTest.java
@@ -3,15 +3,27 @@ package org.folio.okapi.common;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.containsString;
 
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.client.WebClient;
-import org.junit.Test;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.folio.okapi.testing.UtilityClassTester;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-public class WebClientFactoryTest {
+@ExtendWith(VertxExtension.class)
+class WebClientFactoryTest {
 
   @Test
-  public void getWebClient() {
+  void isUtilityClass() {
+    UtilityClassTester.assertUtilityClass(WebClientFactory.class);
+  }
+
+  @Test
+  void getWebClient() {
     Vertx vertxA = Vertx.vertx();
     Vertx vertxB = Vertx.vertx();
     WebClient webClientA1 = WebClientFactory.getWebClient(vertxA);
@@ -21,6 +33,30 @@ public class WebClientFactoryTest {
     assertThat(webClientA1, is(webClientA2));
     assertThat(webClientB1, is(webClientB2));
     assertThat(webClientA1, is(not(webClientB1)));
+  }
+
+  @Test
+  void getWebClientWithOptions(Vertx vertx, VertxTestContext vtc) {
+    vertx.createHttpServer()
+        .requestHandler(request -> request.response().end(request.getHeader("User-Agent")))
+        .listen(8000)
+        .compose(x -> {
+          WebClientOptions options = new WebClientOptions().setUserAgent("Nimbus");
+          WebClient webClient = WebClientFactory.getWebClient(vertx, options);
+          return webClient.getAbs("http://localhost:8000").send();
+        })
+        .onComplete(vtc.succeeding(res -> assertThat(res.bodyAsString(), is("Nimbus"))))
+        .compose(x -> {
+          WebClient webClient = WebClientFactory.getWebClient(Vertx.vertx());  // new Vertx
+          return webClient.getAbs("http://localhost:8000").send();
+        })
+        .onComplete(vtc.succeeding(res -> assertThat(res.bodyAsString(), containsString("Vert"))))
+        .compose(x -> {
+          WebClient webClient = WebClientFactory.getWebClient(vertx);  // first Vertx
+          return webClient.getAbs("http://localhost:8000").send();
+        })
+        .onComplete(vtc.succeeding(res -> assertThat(res.bodyAsString(), is("Nimbus"))))
+        .onSuccess(x -> vtc.completeNow());
   }
 }
 
