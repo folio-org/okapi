f810a66b1aa26771aa00bb9075791abe9fafefb1
Julian Ladisch <julianladisch@users.noreply.github.com>
2022-01-20 15:57:43 +0100

    OKAPI-1068: Asynchronous Loggers (Log4j2 + Disruptor)
    Okapis logging is the bottleneck when GBV bulk loads inventory from the union catalogue. Using default loglevel INFO loads 20,000 instances per hour, using loglevel ERROR loads 220,000 instances per hour. This is more than 10 times as fast.
    
    Log4J allows for asynchronous loggers that execute the logging in a separate thread:
    
    https://logging.apache.org/log4j/2.x/manual/async.html
    https://logging.apache.org/log4j/2.x/performance.html
    
    Enable asynchronous logging by default, with an option to disable it.

--- okapi-core/src/main/java/org/folio/okapi/MainCluster.java
+++ okapi-core/src/main/java/org/folio/okapi/MainCluster.java
@@ -1,6 +1,8 @@
 package org.folio.okapi;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector;
+import org.apache.logging.log4j.core.util.Constants;
 import org.folio.okapi.common.OkapiLogger;
 
 class MainCluster {
@@ -11,6 +13,7 @@ class MainCluster {
   public static void main(String[] args) {
     System.setProperty("vertx.logger-delegate-factory-class-name",
         "io.vertx.core.logging.Log4jLogDelegateFactory");
+    setLog4jContextSelector();
     Logger logger = OkapiLogger.get();
     MainDeploy d = new MainDeploy();
     d.init(args, res -> {
@@ -19,5 +22,14 @@ class MainCluster {
         System.exit(1);
       }
     });
+  }
+
+  static void setLog4jContextSelector() {
+    if (System.getProperty(Constants.LOG4J_CONTEXT_SELECTOR) != null) {
+      return;
+    }
+    // set default: asynchronous loggers for low-latency logging
+    System.setProperty(Constants.LOG4J_CONTEXT_SELECTOR,
+        AsyncLoggerContextSelector.class.getName());
   }
 }
