30fc07aa49431043b1ae348d2617b53406f320ea
Adam Dickmeiss <adam@indexdata.dk>
2022-02-05 15:56:10 +0100

    Discovery of services in Kubernetes OKAPI-1045 (#1185)
    Co-authored-by: julianladisch <julianladisch@users.noreply.github.com>

--- okapi-core/src/main/java/org/folio/okapi/ConfNames.java
+++ okapi-core/src/main/java/org/folio/okapi/ConfNames.java
@@ -16,6 +16,12 @@ public final class ConfNames {
   public static final String DOCKER_URL = "dockerUrl";
   public static final String ENABLE_TRACE_HEADERS = "trace_headers";
   public static final String ENABLE_SYSTEM_AUTH = "enable_system_auth";
+  public static final String KUBE_CONFIG = "kube_config";
+  public static final String KUBE_TOKEN = "kube_token";
+  public static final String KUBE_SERVER_URL = "kube_server_url";
+  public static final String KUBE_SERVER_PEM = "kube_server_pem";
+  public static final String KUBE_NAMESPACE = "kube_namespace";
+  public static final String KUBE_REFRESH_INTERVAL = "kube_refresh_interval";
 
   private ConfNames() {
     throw new UnsupportedOperationException("Cannot instantiate utility class.");
