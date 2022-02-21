5a88b8cca6b0751993a4f3a701111132d99e8d66
Adam Dickmeiss <adam@indexdata.dk>
2022-02-21 19:43:13 +0100

    Revert a bit

--- okapi-core/src/test/java/org/folio/okapi/util/DepResolutionTest.java
+++ okapi-core/src/test/java/org/folio/okapi/util/DepResolutionTest.java
@@ -900,7 +900,7 @@ public class DepResolutionTest {
 
     // patch to higher version with st given, but with multiple products to support it
     {
-      ModuleDescriptor ot101_alt = new ModuleDescriptor("ot-1.0.1");
+      ModuleDescriptor ot101_alt = new ModuleDescriptor("otA-1.0.1");
       ot101_alt.setRequires(ot101.getRequires());
 
       ModuleDescriptor os101_alt = new ModuleDescriptor("os-1.0.1");
