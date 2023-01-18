f196073db8eb2127d8f18beb0a57c09b3d1136fa
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-01-18 21:53:31 +0100

    OKAPI-1155: HTTP connection reset before response is sent
    ProxyService sometimes resets the HTTP connection before the
    response has been sent. This may happen because Netty sends
    the response asynchronously and the requestor may pause while
    handling other work.
    
    (cherry picked from commit 981f7d7a11b262218d259cc674054b6124e476c1)

--- okapi-core/src/main/java/org/folio/okapi/managers/ProxyService.java
+++ okapi-core/src/main/java/org/folio/okapi/managers/ProxyService.java
@@ -647,7 +647,6 @@ public class ProxyService {
       logger.trace("ProxyRequestHttpClient request buf '{}'", bcontent);
       clientsEnd(bcontent, clientRequestList);
       clientRequest.end(bcontent);
-      ctx.response().closeHandler(x -> clientRequest.setTimeout(1));
       log(clientRequest);
       clientRequest.response()
           .onFailure(res -> proxyClientFailure(pc, mi, requestOptions, res))
@@ -845,7 +844,6 @@ public class ProxyService {
         }
         streamHandle(stream, clientRequest, clientRequestList);
       }
-      ctx.response().closeHandler(x -> clientRequest.setTimeout(1));
       log(clientRequest);
       clientRequest.response()
           .onFailure(res -> proxyClientFailure(pc, mi, requestOptions, res))
@@ -885,7 +883,6 @@ public class ProxyService {
       final Timer.Sample sample = MetricsHelper.getTimerSample();
       copyHeaders(clientRequest, ctx, mi);
       clientRequest.end();
-      ctx.response().closeHandler(x -> clientRequest.setTimeout(1));
       log(clientRequest);
       clientRequest.response()
           .onFailure(res -> proxyClientFailure(pc, mi, requestOptions, res))
