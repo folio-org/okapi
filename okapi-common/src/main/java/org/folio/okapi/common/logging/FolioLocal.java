50249849d8921758204e1c6d19ac8e79422a6ed8
Julian Ladisch <julianladisch@users.noreply.github.com>
2025-11-25 11:07:09 +0100

    Fix javadoc smells

--- okapi-common/src/main/java/org/folio/okapi/common/logging/FolioLocal.java
+++ okapi-common/src/main/java/org/folio/okapi/common/logging/FolioLocal.java
@@ -16,16 +16,17 @@ public class FolioLocal implements VertxServiceProvider {
    * {@code src/main/resources/META-INF/services/io.vertx.core.spi.VertxServiceProvider}
    * to prevent a timing (race condition) issue:
    * <a href="https://folio-org.atlassian.net/browse/OKAPI-1228">OKAPI-1228</a>.
-   * <p>
-   * If some other dependency also ships with io.vertx.core.spi.VertxServiceProvider
+   *
+   * <p>If some other dependency also ships with io.vertx.core.spi.VertxServiceProvider
    * add the {@code ServicesResourceTransformer} transformer to {@code maven-shade-plugin}
    * so that both files get concatenated, see
-   * <pre><a href="https://maven.apache.org/plugins/maven-shade-plugin/examples/resource-transformers.html
-     #ServicesResourceTransformer">manual</a></pre>
+   * <a href="https://maven.apache.org/plugins/maven-shade-plugin/examples/resource-transformers.html#ServicesResourceTransformer">manual</a>
    * and <a href="https://github.com/folio-org/mod-copycat/pull/123/files">example</a>.
    */
   @Override
-  @SuppressWarnings("java:S2696")  // Suppress "Make method static", cannot change supertype
+  @SuppressWarnings({"java:S2696",  // Suppress "Make method static", cannot change supertype
+      "java:S103",  // Suppress "Line too long" to keep the URL in the javadoc without breaking it
+  })
   public void init(VertxBootstrap builder) {
     initialized = true;
   }
